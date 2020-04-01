137f7bff8f0b76ed024800897e35f0eb6bc8fa32
jhernandezb <contact@jhernandez.me>
2020-03-31 18:44:53 -0600

    added build script

---
+++ contrib/build.sh
@@ -0,0 +1,2 @@
+GOARCH=amd64 GOOS=linux go build -o bin/rocketd github.com/rocket-protocol/rocketzone/cmd/rocketd
+GOARCH=amd64 GOOS=linux  go build -o bin/rocketcli github.com/rocket-protocol/rocketzone/cmd/rocketcli
