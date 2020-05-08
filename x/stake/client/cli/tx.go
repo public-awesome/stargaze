0545a90fbbe7cc36af5ad682b4bb50917b47b514
Shane Vitarana <shanev@gmail.com>
2020-05-07 20:37:45 -0700

    removed old imports

--- x/stake/client/cli/tx.go
+++ x/stake/client/cli/tx.go
@@ -1,73 +1,55 @@
 package cli
 
-import (
-	"bufio"
-	"fmt"
-	"strings"
+// // GetTxCmd returns the transaction commands for this module
+// func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+// 	stakeTxCmd := &cobra.Command{
+// 		Use:                        types.ModuleName,
+// 		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+// 		DisableFlagParsing:         true,
+// 		SuggestionsMinimumDistance: 2,
+// 		RunE:                       client.ValidateCmd,
+// 	}
 
-	"github.com/spf13/cobra"
+// 	stakeTxCmd.AddCommand(flags.PostCommands(
+// 		GetCmdDelegate(cdc),
+// 	)...)
 
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/client/context"
-	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/codec"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/version"
-	"github.com/cosmos/cosmos-sdk/x/auth"
-	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	"github.com/rocket-protocol/stakebird/x/stake/types"
-)
+// 	return stakeTxCmd
+// }
 
-// GetTxCmd returns the transaction commands for this module
-func GetTxCmd(cdc *codec.Codec) *cobra.Command {
-	stakeTxCmd := &cobra.Command{
-		Use:                        types.ModuleName,
-		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
-		DisableFlagParsing:         true,
-		SuggestionsMinimumDistance: 2,
-		RunE:                       client.ValidateCmd,
-	}
+// // GetCmdDelegate implements the delegate command.
+// func GetCmdDelegate(cdc *codec.Codec) *cobra.Command {
+// 	return &cobra.Command{
+// 		Use:   "delegate [validator-addr] [amount]",
+// 		Args:  cobra.ExactArgs(2),
+// 		Short: "Delegate liquid tokens to a validator",
+// 		Long: strings.TrimSpace(
+// 			fmt.Sprintf(`Delegate an amount of liquid coins to a validator from your wallet.
+// Example:
+// $ %s tx stake delegate cosmosvaloper1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 1000stake --from mykey
+// `,
+// 				version.ClientName,
+// 			),
+// 		),
+// 		RunE: func(cmd *cobra.Command, args []string) error {
+// 			inBuf := bufio.NewReader(cmd.InOrStdin())
+// 			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(auth.DefaultTxEncoder(cdc))
+// 			cliCtx := context.NewCLIContextWithInput(inBuf).WithCodec(cdc)
 
-	stakeTxCmd.AddCommand(flags.PostCommands(
-		GetCmdDelegate(cdc),
-	)...)
+// 			// amount, err := sdk.ParseCoin(args[1])
+// 			// if err != nil {
+// 			// 	return err
+// 			// }
 
-	return stakeTxCmd
-}
+// 			// delAddr := cliCtx.GetFromAddress()
+// 			// valAddr, err := sdk.ValAddressFromBech32(args[0])
+// 			// if err != nil {
+// 			// 	return err
+// 			// }
 
-// GetCmdDelegate implements the delegate command.
-func GetCmdDelegate(cdc *codec.Codec) *cobra.Command {
-	return &cobra.Command{
-		Use:   "delegate [validator-addr] [amount]",
-		Args:  cobra.ExactArgs(2),
-		Short: "Delegate liquid tokens to a validator",
-		Long: strings.TrimSpace(
-			fmt.Sprintf(`Delegate an amount of liquid coins to a validator from your wallet.
-Example:
-$ %s tx stake delegate cosmosvaloper1l2rsakp388kuv9k8qzq6lrm9taddae7fpx59wm 1000stake --from mykey
-`,
-				version.ClientName,
-			),
-		),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			inBuf := bufio.NewReader(cmd.InOrStdin())
-			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(auth.DefaultTxEncoder(cdc))
-			cliCtx := context.NewCLIContextWithInput(inBuf).WithCodec(cdc)
-
-			// amount, err := sdk.ParseCoin(args[1])
-			// if err != nil {
-			// 	return err
-			// }
-
-			// delAddr := cliCtx.GetFromAddress()
-			// valAddr, err := sdk.ValAddressFromBech32(args[0])
-			// if err != nil {
-			// 	return err
-			// }
-
-			// msg := types.NewMsgDelegate(delAddr, valAddr, amount)
-			// return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
-			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{})
-		},
-	}
-}
+// 			// msg := types.NewMsgDelegate(delAddr, valAddr, amount)
+// 			// return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+// 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{})
+// 		},
+// 	}
+// }
--- x/stake/client/rest/tx.go
+++ x/stake/client/rest/tx.go
@@ -1,63 +1,51 @@
 package rest
 
-import (
-	"bytes"
-	"net/http"
+// func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
+// 	r.HandleFunc(
+// 		"/stake/{delegatorAddr}/delegations",
+// 		postDelegationsHandlerFn(cliCtx),
+// 	).Methods("POST")
+// }
 
-	"github.com/gorilla/mux"
+// // DelegateRequest defines the properties of a delegation request's body.
+// type DelegateRequest struct {
+// 	BaseReq          rest.BaseReq   `json:"base_req" yaml:"base_req"`
+// 	DelegatorAddress sdk.AccAddress `json:"delegator_address" yaml:"delegator_address"` // in bech32
+// 	ValidatorAddress sdk.ValAddress `json:"validator_address" yaml:"validator_address"` // in bech32
+// 	Amount           sdk.Coin       `json:"amount" yaml:"amount"`
+// }
 
-	"github.com/cosmos/cosmos-sdk/client/context"
-	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/types/rest"
-	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-)
+// func postDelegationsHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		var req DelegateRequest
 
-func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc(
-		"/stake/{delegatorAddr}/delegations",
-		postDelegationsHandlerFn(cliCtx),
-	).Methods("POST")
-}
+// 		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+// 			return
+// 		}
 
-// DelegateRequest defines the properties of a delegation request's body.
-type DelegateRequest struct {
-	BaseReq          rest.BaseReq   `json:"base_req" yaml:"base_req"`
-	DelegatorAddress sdk.AccAddress `json:"delegator_address" yaml:"delegator_address"` // in bech32
-	ValidatorAddress sdk.ValAddress `json:"validator_address" yaml:"validator_address"` // in bech32
-	Amount           sdk.Coin       `json:"amount" yaml:"amount"`
-}
+// 		req.BaseReq = req.BaseReq.Sanitize()
+// 		if !req.BaseReq.ValidateBasic(w) {
+// 			return
+// 		}
 
-func postDelegationsHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		var req DelegateRequest
+// 		// msg := types.NewMsgDelegate(req.DelegatorAddress, req.ValidatorAddress, req.Amount)
+// 		// if err := msg.ValidateBasic(); err != nil {
+// 		// 	rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+// 		// 	return
+// 		// }
 
-		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
-			return
-		}
+// 		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
+// 		if err != nil {
+// 			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
+// 			return
+// 		}
 
-		req.BaseReq = req.BaseReq.Sanitize()
-		if !req.BaseReq.ValidateBasic(w) {
-			return
-		}
+// 		if !bytes.Equal(fromAddr, req.DelegatorAddress) {
+// 			rest.WriteErrorResponse(w, http.StatusUnauthorized, "must use own delegator address")
+// 			return
+// 		}
 
-		// msg := types.NewMsgDelegate(req.DelegatorAddress, req.ValidatorAddress, req.Amount)
-		// if err := msg.ValidateBasic(); err != nil {
-		// 	rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-		// 	return
-		// }
-
-		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		if !bytes.Equal(fromAddr, req.DelegatorAddress) {
-			rest.WriteErrorResponse(w, http.StatusUnauthorized, "must use own delegator address")
-			return
-		}
-
-		// utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
-		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{})
-	}
-}
+// 		// utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
+// 		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{})
+// 	}
+// }
