8262dd01c7ccf84f3981e074ebe1d232b288313a
Shane Vitarana <shanev@gmail.com>
2020-05-09 22:16:03 -0700

    delegation test..

--- x/stake/keeper/delegation_test.go
+++ x/stake/keeper/delegation_test.go
@@ -2,12 +2,17 @@ package keeper_test
 
 import (
 	"testing"
+
+	"github.com/cosmos/cosmos-sdk/simapp"
 )
 
 func TestDelegation(t *testing.T) {
-	// _, app, ctx := createTestInput()
+	_, app, ctx := createTestInput()
 
-	// addrDels := simapp.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(10000))
+	addrDels := simapp.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(10000))
 	// valAddrs := simapp.ConvertAddrsToValAddrs(addrDels)
 
+	vendorID := uint64(100)
+	postID := uint64(200)
+	app.StakeKeeper.Delegate(ctx, vendorID, postID, delAddr, valAddr, votingPeriod, amount)
 }
