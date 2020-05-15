6aba46e4c3a422dc9f35dbd3663fc17fc995a5c8
Shane Vitarana <shanev@gmail.com>
2020-05-15 13:15:33 -0700

    renamed rocket to stake for stakebird branding

--- contrib/build.sh
+++ contrib/build.sh
@@ -1,2 +1,2 @@
-GOARCH=amd64 GOOS=linux go build -o bin/rocketd github.com/rocket-protocol/stakebird/cmd/rocketd
-GOARCH=amd64 GOOS=linux  go build -o bin/rocketcli github.com/rocket-protocol/stakebird/cmd/rocketcli
+GOARCH=amd64 GOOS=linux go build -o bin/rocketd github.com/rocket-protocol/stakebird/cmd/staked
+GOARCH=amd64 GOOS=linux  go build -o bin/rocketcli github.com/rocket-protocol/stakebird/cmd/stakecli
