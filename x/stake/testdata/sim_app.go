c7542cb56a963d7ad1d0ab4ed33aeb797c2a6663
Shane Vitarana <shanev@gmail.com>
2020-05-15 17:39:12 -0700

    changed address prefix from cosmos to stake

--- x/stake/testdata/sim_app.go
+++ x/stake/testdata/sim_app.go
@@ -49,7 +49,7 @@ const (
 	DefaultStakeDenom = "ufuel"
 
 	// Bech32MainPrefix defines the Bech32 prefix of an account's address
-	Bech32MainPrefix = "cosmos"
+	Bech32MainPrefix = "stake"
 
 	// PrefixAccount is the prefix for account keys
 	PrefixAccount = "acc"
