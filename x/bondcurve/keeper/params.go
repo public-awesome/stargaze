3eebfc74216da7d1ac242398a900a3d87668f51a
Shane Vitarana <shanev@gmail.com>
2020-05-22 17:07:09 -0700

    scaffolded bondcurve module

---
+++ x/bondcurve/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/rocket-protocol/stakebird/x/bondcurve/types"
+)
+
+// GetParams returns the total set of bondcurve parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the bondcurve parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
