df5332117ec6831cbb759b69852056d38f835255
Shane Vitarana <shanev@gmail.com>
2020-07-09 00:16:17 -0700

    Integrated Peggy modules ethbridge + oracle (#93)

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/public-awesome/stakebird
 go 1.14
 
 require (
+	github.com/allegro/bigcache v1.2.1 // indirect
 	github.com/cosmos/cosmos-sdk v0.34.4-0.20200530180557-ba70f4d4dc2e
 	github.com/davecgh/go-spew v1.1.1
+	github.com/ethereum/go-ethereum v1.8.27
 	github.com/gogo/protobuf v1.3.1
 	github.com/golang/protobuf v1.4.1
 	github.com/gorilla/mux v1.7.4
@@ -15,5 +17,7 @@ require (
 	github.com/tendermint/go-amino v0.15.1
 	github.com/tendermint/tendermint v0.33.4
 	github.com/tendermint/tm-db v0.5.1
+	golang.org/x/crypto v0.0.0-20200707235045-ab33eee955e0 // indirect
+	golang.org/x/sys v0.0.0-20200625212154-ddb9806d33ae // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
---
+++ x/ethbridge/handler.go
@@ -0,0 +1,144 @@
+//nolint:dupl
+package ethbridge
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/public-awesome/stakebird/x/ethbridge/types"
+	"github.com/public-awesome/stakebird/x/oracle"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+)
+
+// NewHandler returns a handler for "ethbridge" type messages.
+func NewHandler(
+	accountKeeper types.AccountKeeper, bridgeKeeper Keeper,
+	cdc *codec.Codec) sdk.Handler {
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+		switch msg := msg.(type) {
+		case MsgCreateEthBridgeClaim:
+			return handleMsgCreateEthBridgeClaim(ctx, cdc, bridgeKeeper, msg)
+		case MsgBurn:
+			return handleMsgBurn(ctx, cdc, accountKeeper, bridgeKeeper, msg)
+		case MsgLock:
+			return handleMsgLock(ctx, cdc, accountKeeper, bridgeKeeper, msg)
+		default:
+			errMsg := fmt.Sprintf("unrecognized ethbridge message type: %v", msg.Type())
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
+		}
+	}
+}
+
+// Handle a message to create a bridge claim
+func handleMsgCreateEthBridgeClaim(
+	ctx sdk.Context, cdc *codec.Codec, bridgeKeeper Keeper, msg MsgCreateEthBridgeClaim,
+) (*sdk.Result, error) {
+	status, err := bridgeKeeper.ProcessClaim(ctx, types.EthBridgeClaim(msg))
+	if err != nil {
+		return nil, err
+	}
+	if status.Text == oracle.SuccessStatusText {
+		if err = bridgeKeeper.ProcessSuccessfulClaim(ctx, status.FinalClaim); err != nil {
+			return nil, err
+		}
+	}
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.ValidatorAddress.String()),
+		),
+		sdk.NewEvent(
+			types.EventTypeCreateClaim,
+			sdk.NewAttribute(types.AttributeKeyEthereumSender, msg.EthereumSender.String()),
+			sdk.NewAttribute(types.AttributeKeyCosmosReceiver, msg.CosmosReceiver.String()),
+			sdk.NewAttribute(types.AttributeKeyAmount, strconv.FormatInt(msg.Amount, 10)),
+			sdk.NewAttribute(types.AttributeKeySymbol, msg.Symbol),
+			sdk.NewAttribute(types.AttributeKeyTokenContract, msg.TokenContractAddress.String()),
+			sdk.NewAttribute(types.AttributeKeyClaimType, msg.ClaimType.String()),
+		),
+		sdk.NewEvent(
+			types.EventTypeProphecyStatus,
+			sdk.NewAttribute(types.AttributeKeyStatus, status.Text.String()),
+		),
+	})
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+}
+
+func handleMsgBurn(
+	ctx sdk.Context, cdc *codec.Codec, accountKeeper types.AccountKeeper,
+	bridgeKeeper Keeper, msg MsgBurn,
+) (*sdk.Result, error) {
+
+	account := accountKeeper.GetAccount(ctx, msg.CosmosSender)
+	if account == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.CosmosSender.String())
+	}
+
+	coins := sdk.NewCoins(sdk.NewInt64Coin(msg.Symbol, msg.Amount))
+	if err := bridgeKeeper.ProcessBurn(ctx, msg.CosmosSender, coins); err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.CosmosSender.String()),
+		),
+		sdk.NewEvent(
+			types.EventTypeBurn,
+			sdk.NewAttribute(types.AttributeKeyEthereumChainID, strconv.Itoa(msg.EthereumChainID)),
+			sdk.NewAttribute(types.AttributeKeyCosmosSender, msg.CosmosSender.String()),
+			sdk.NewAttribute(types.AttributeKeyEthereumReceiver, msg.EthereumReceiver.String()),
+			sdk.NewAttribute(types.AttributeKeyAmount, strconv.FormatInt(msg.Amount, 10)),
+			sdk.NewAttribute(types.AttributeKeySymbol, msg.Symbol),
+			sdk.NewAttribute(types.AttributeKeyCoins, coins.String()),
+		),
+	})
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+
+}
+
+func handleMsgLock(
+	ctx sdk.Context, cdc *codec.Codec, accountKeeper types.AccountKeeper,
+	bridgeKeeper Keeper, msg MsgLock,
+) (*sdk.Result, error) {
+
+	account := accountKeeper.GetAccount(ctx, msg.CosmosSender)
+	if account == nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, msg.CosmosSender.String())
+	}
+
+	coins := sdk.NewCoins(sdk.NewInt64Coin(msg.Symbol, msg.Amount))
+	if err := bridgeKeeper.ProcessLock(ctx, msg.CosmosSender, coins); err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.CosmosSender.String()),
+		),
+		sdk.NewEvent(
+			types.EventTypeLock,
+			sdk.NewAttribute(types.AttributeKeyEthereumChainID, strconv.Itoa(msg.EthereumChainID)),
+			sdk.NewAttribute(types.AttributeKeyCosmosSender, msg.CosmosSender.String()),
+			sdk.NewAttribute(types.AttributeKeyEthereumReceiver, msg.EthereumReceiver.String()),
+			sdk.NewAttribute(types.AttributeKeyAmount, strconv.FormatInt(msg.Amount, 10)),
+			sdk.NewAttribute(types.AttributeKeySymbol, msg.Symbol),
+			sdk.NewAttribute(types.AttributeKeyCoins, coins.String()),
+		),
+	})
+
+	return &sdk.Result{Events: ctx.EventManager().ABCIEvents()}, nil
+
+}
