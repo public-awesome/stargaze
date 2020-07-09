0b3e557d83afe9cbab082e1f51c06237447f9764
jhernandezb <contact@jhernandez.me>
2020-07-09 13:29:18 -0600

    add gaia config

--- contrib/ci/setup-gaia.sh
+++ contrib/ci/setup-gaia.sh
@@ -1,8 +1,23 @@
 GAIA_TAG="goz-phase-3"
+DENOM=stake
 
+# install gaia
 git clone https://github.com/cosmos/gaia
 cd gaia 
 git fetch --tags origin 
 git checkout $GAIA_TAG
 make install
 gaiad version --long
+
+
+
+# Setup gaia
+gaiad init --chain-id $CHAINID $CHAINID
+sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' ~/.gaiad/config/config.toml
+sed -i "s/\"stake\"/\"$DENOM\"/g" ~/.gaiad/config/genesis.json
+sed -i 's/pruning = "syncable"/pruning = "nothing"/g' ~/.gaiad/config/app.toml
+gaiacli keys --keyring-backend test add validator
+
+gaiad add-genesis-account $(gaiacli keys --keyring-backend test show validator -a) 100000000000$DENOM
+gaiad gentx --name validator --keyring-backend test --amount 900000000$DENOM
+gaiad collect-gentxs
