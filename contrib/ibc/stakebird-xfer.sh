f3888b6e48118433e41920a4e5580825014473fc
jhernandezb <contact@jhernandez.me>
2020-07-09 22:20:24 -0600

    enable transfer commands in setup script

--- contrib/ibc/stakebird-xfer.sh
+++ contrib/ibc/stakebird-xfer.sh
@@ -43,15 +43,15 @@ rly lite init ibc1 -f
 tree ~/.relayer
 
 # Now you can connect the two chains with one command:
-# rly -d tx link demo
+rly -d tx link demo
 
 # Check the token balances on both chains
-# rly q bal ibc0
-# rly q bal ibc1
+rly q bal ibc0
+rly q bal ibc1
 
 # Then send some tokens between the chains
-# rly tx xfer ibc0 ibc1 100000stake true $(rly keys show ibc1 testkey)
+rly tx xfer ibc0 ibc1 100000stake true $(rly keys show ibc1 testkey)
 
 # See that the transfer has completed
-# rly q bal ibc0
-# rly q bal ibc1
+rly q bal ibc0
+rly q bal ibc1
