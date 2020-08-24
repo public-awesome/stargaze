b193e600a4192c8181a87bc0d968fccd9ee1e371
Mohit Mamoria <mohit.mamoria@gmail.com>
2020-08-24 13:50:01 +0530

    added some tests

---
+++ x/curating/keeper/keeper_test.go
@@ -0,0 +1,12 @@
+package keeper_test
+
+import (
+	"testing"
+)
+
+func TestNewKeeper(t *testing.T) {
+	// _, app, ctx := testdata.CreateTestInput()
+
+	// app.CuratingKeeper.
+
+}
--- x/curating/keeper/post_test.go
+++ x/curating/keeper/post_test.go
@@ -1,7 +1,10 @@
 package keeper_test
 
 import (
+	"crypto/sha256"
 	"testing"
+
+	curatingTypes "github.com/public-awesome/stakebird/x/curating/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/public-awesome/stakebird/testdata"
@@ -98,4 +101,55 @@ func TestPost_WithRewardAccount(t *testing.T) {
 
 	vps := app.CuratingKeeper.GetCurationQueueTimeSlice(ctx, ctx.BlockTime())
 	require.NotNil(t, vps)
+}
+
+func TestDeletePost(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	deposit := sdk.NewInt64Coin("ustb", 1000000)
+	addrs := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+
+	err := app.CuratingKeeper.CreatePost(ctx, vendorID, postID, "body string", deposit, addrs[0], addrs[1])
+	require.NoError(t, err)
+
+	_, found, err := app.CuratingKeeper.GetPost(ctx, vendorID, postID)
+	require.NoError(t, err)
+	require.True(t, found, "post should be found")
+
+	postIDHash, err := hash(postID)
+	require.NoError(t, err)
+	err = app.CuratingKeeper.DeletePost(ctx, vendorID, postIDHash)
+	require.NoError(t, err)
+
+	_, found, err = app.CuratingKeeper.GetPost(ctx, vendorID, postID)
+	require.NoError(t, err)
+	require.False(t, found, "post should NOT be found")
+}
+
+func TestInsertCurationQueue(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	postIDHash, err := hash(postID)
+	require.NoError(t, err)
+
+	curationWindow := app.CuratingKeeper.GetParams(ctx).CurationWindow
+	curationEndTime := ctx.BlockTime().Add(curationWindow)
+	app.CuratingKeeper.InsertCurationQueue(ctx, vendorID, postIDHash, curationEndTime)
+
+	timeSlice := app.CuratingKeeper.GetCurationQueueTimeSlice(ctx, curationEndTime)
+	require.Len(t, timeSlice, 1)
+
+	vpPair := curatingTypes.VPPair{VendorID: vendorID, PostIDHash: postIDHash}
+	require.Equal(t, vpPair, timeSlice[0])
+}
+
+func hash(body string) ([]byte, error) {
+	h := sha256.New()
+	_, err := h.Write([]byte(body))
+	if err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
 }
