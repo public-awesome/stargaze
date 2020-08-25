55a0218eecf58298c5ff0fda9b3bf5e9e0465d2c
Mohit Mamoria <mohit.mamoria@gmail.com>
2020-08-25 18:34:46 +0530

    added msg upvote tests

--- x/curating/types/msg_test.go
+++ x/curating/types/msg_test.go
@@ -97,3 +97,105 @@ func TestValidateBasicMsgPost_InvalidVendorID(t *testing.T) {
 	err := msg.ValidateBasic()
 	require.NotNil(t, err)
 }
+
+func TestNewMsgUpvote(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	postID := "100"
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+	voteNum := int32(2)
+
+	msg := types.NewMsgUpvote(vendorID, postID, addresses[0], addresses[0], voteNum)
+	require.Equal(t, msg.GetVendorID(), vendorID)
+	require.Equal(t, msg.GetPostID(), postID)
+	require.Equal(t, msg.GetCurator(), addresses[0])
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+	require.Equal(t, msg.GetVoteNum(), voteNum)
+}
+
+func TestValidateBasicMsgUpvote_EmptyCurator(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	postID := "100"
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+	voteNum := int32(2)
+
+	msg := types.NewMsgUpvote(vendorID, postID, nil, addresses[0], voteNum)
+	require.Equal(t, msg.GetVendorID(), vendorID)
+	require.Equal(t, msg.GetPostID(), postID)
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+	require.Equal(t, msg.GetVoteNum(), voteNum)
+
+	err := msg.ValidateBasic()
+	require.NotNil(t, err)
+}
+
+func TestValidateBasicMsgUpvote_EmptyPostID(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	postID := ""
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+	voteNum := int32(2)
+
+	msg := types.NewMsgUpvote(vendorID, postID, addresses[0], addresses[0], voteNum)
+	require.Equal(t, msg.GetVendorID(), vendorID)
+	require.Equal(t, msg.GetCurator(), addresses[0])
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+	require.Equal(t, msg.GetVoteNum(), voteNum)
+
+	err := msg.ValidateBasic()
+	require.NotNil(t, err)
+}
+
+func TestValidateBasicMsgUpvote_InvalidVendorID(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(0)
+	postID := "100"
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+	voteNum := int32(2)
+
+	msg := types.NewMsgUpvote(vendorID, postID, addresses[0], addresses[0], voteNum)
+	require.Equal(t, msg.GetPostID(), postID)
+	require.Equal(t, msg.GetCurator(), addresses[0])
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+	require.Equal(t, msg.GetVoteNum(), voteNum)
+
+	err := msg.ValidateBasic()
+	require.NotNil(t, err)
+}
+
+func TestValidateBasicMsgUpvote_InvalidVoteNum(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	vendorID := uint32(1)
+	postID := "100"
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+
+	// zero votes
+	voteNum := int32(0)
+
+	msg := types.NewMsgUpvote(vendorID, postID, addresses[0], addresses[0], voteNum)
+	require.Equal(t, msg.GetVendorID(), vendorID)
+	require.Equal(t, msg.GetPostID(), postID)
+	require.Equal(t, msg.GetCurator(), addresses[0])
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+
+	err := msg.ValidateBasic()
+	require.NotNil(t, err)
+
+	// negative votes
+	voteNum = int32(-1)
+
+	msg = types.NewMsgUpvote(vendorID, postID, addresses[0], addresses[0], voteNum)
+	require.Equal(t, msg.GetVendorID(), vendorID)
+	require.Equal(t, msg.GetPostID(), postID)
+	require.Equal(t, msg.GetCurator(), addresses[0])
+	require.Equal(t, msg.GetRewardAccount(), addresses[0])
+
+	err = msg.ValidateBasic()
+	require.NotNil(t, err)
+}
