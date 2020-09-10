7022f1c1eb484fd6e16220857a97e68cfc06ed1a
Mohit Mamoria <mohit.mamoria@gmail.com>
2020-09-10 15:57:56 +0530

    added some tests and checks

--- x/user/keeper/vouch.go
+++ x/user/keeper/vouch.go
@@ -1,13 +1,16 @@
 package keeper
 
 import (
+	"fmt"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/public-awesome/stakebird/x/user/types"
 )
 
 // GetVouchesByVoucher returns the vouch if one exists
 func (k Keeper) GetVouchesByVoucher(
-	ctx sdk.Context, voucher sdk.AccAddress) (vouches []types.Vouch, err error) {
+	ctx sdk.Context, voucher sdk.AccAddress) (vouches []types.Vouch) {
 
 	store := ctx.KVStore(k.storeKey)
 
@@ -21,7 +24,7 @@ func (k Keeper) GetVouchesByVoucher(
 		vouches = append(vouches, vouch)
 	}
 
-	return vouches, nil
+	return vouches
 }
 
 // GetVouchByVouched returns the vouch if one exists
@@ -42,22 +45,48 @@ func (k Keeper) GetVouchByVouched(
 
 // IsVouched returns whether the given address has been previously vouched for
 func (k Keeper) IsVouched(
-	ctx sdk.Context, address sdk.AccAddress) (vouched bool, err error) {
+	ctx sdk.Context, address sdk.AccAddress) (vouched bool) {
 
 	store := ctx.KVStore(k.storeKey)
 
 	key := types.VouchedKey(address)
 	value := store.Get(key)
 	if value == nil {
-		return false, nil
+		return false
 	}
 
-	return true, nil
+	return true
+}
+
+// CanVouch returns whether the given address can vouch for someone
+func (k Keeper) CanVouch(
+	ctx sdk.Context, address sdk.AccAddress) (can bool) {
+
+	vouches := k.GetVouchesByVoucher(ctx, address)
+
+	// TODO: check the condition for the threshold amount
+
+	// if already vouched enough time
+	if uint32(len(vouches)) < k.GetParams(ctx).VouchCount {
+		return true
+	}
+
+	return false
 }
 
 // CreateVouch registers a vouch on-chain.
 func (k Keeper) CreateVouch(
 	ctx sdk.Context, voucher, vouched sdk.AccAddress, comment string) error {
+
+	if !k.CanVouch(ctx, voucher) {
+		return sdkerrors.Wrap(
+			sdkerrors.ErrInvalidRequest, fmt.Sprintf("given voucher cannot vouch %x", voucher))
+	}
+
+	if k.IsVouched(ctx, vouched) {
+		return sdkerrors.Wrap(
+			sdkerrors.ErrInvalidRequest, fmt.Sprintf("given account is already vouched for %x", vouched))
+	}
 
 	vouch := types.NewVouch(
 		voucher, vouched, comment,
--- x/user/keeper/vouch_test.go
+++ x/user/keeper/vouch_test.go
@@ -22,7 +22,44 @@ func TestVouch(t *testing.T) {
 	require.NoError(t, err)
 	require.True(t, found, "vouch should be found")
 
-	vouchesByVoucher, err := app.UserKeeper.GetVouchesByVoucher(ctx, voucher)
+	vouchesByVoucher := app.UserKeeper.GetVouchesByVoucher(ctx, voucher)
 	require.NoError(t, err)
 	require.Contains(t, vouchesByVoucher, vouchByVouched, "vouch should be found")
+}
+
+func TestIsVouched(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 3, sdk.NewInt(1000000))
+	voucher := addresses[0]
+	vouched := addresses[1]
+
+	is := app.UserKeeper.IsVouched(ctx, vouched)
+	require.False(t, is)
+
+	err := app.UserKeeper.CreateVouch(ctx, voucher, addresses[1], "")
+	require.NoError(t, err)
+
+	is = app.UserKeeper.IsVouched(ctx, vouched)
+	require.True(t, is)
+}
+
+func TestCanVouch(t *testing.T) {
+	_, app, ctx := testdata.CreateTestInput()
+
+	addresses := testdata.AddTestAddrsIncremental(app, ctx, 4, sdk.NewInt(1000000))
+	voucher := addresses[0]
+
+	can := app.UserKeeper.CanVouch(ctx, voucher)
+	require.True(t, can)
+
+	err := app.UserKeeper.CreateVouch(ctx, voucher, addresses[1], "")
+	require.NoError(t, err)
+	err = app.UserKeeper.CreateVouch(ctx, voucher, addresses[2], "")
+	require.NoError(t, err)
+	err = app.UserKeeper.CreateVouch(ctx, voucher, addresses[3], "")
+	require.NoError(t, err)
+
+	can = app.UserKeeper.CanVouch(ctx, voucher)
+	require.False(t, can)
 }
