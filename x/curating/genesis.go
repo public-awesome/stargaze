4ba4d182cc3cc70da05d50282c3a096c4fd15e0b
Shane Vitarana <shanev@gmail.com>
2020-09-11 01:04:53 -0400

    Mint reward pool coins in genesis

--- x/curating/genesis.go
+++ x/curating/genesis.go
@@ -9,8 +9,10 @@ import (
 func InitGenesis(ctx sdk.Context, k Keeper, data GenesisState) (res []abci.ValidatorUpdate) {
 	k.SetParams(ctx, data.Params)
 
-	// creates the reward pool if it doesn't exist
-	k.GetRewardPool(ctx)
+	err := k.InitializeRewardPool(ctx, k.GetParams(ctx).InitialRewardPool)
+	if err != nil {
+		panic(err)
+	}
 
 	return []abci.ValidatorUpdate{}
 }
