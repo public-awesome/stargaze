7946b42b39d92477e856030f0af1362e6bd97f9b
jhernandezb <contact@jhernandez.me>
2020-09-28 10:36:47 -0600

    add test

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/spf13/cast v1.3.1
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.7.1
+	github.com/stretchr/testify v1.6.1
 	github.com/tendermint/tendermint v0.34.0-rc3.0.20200907055413-3359e0bf2f84
 	github.com/tendermint/tm-db v0.6.2
 	google.golang.org/genproto v0.0.0-20200825200019-8632dd797987
---
+++ x/curating/quadratic_test.go
@@ -0,0 +1,61 @@
+package curating_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/public-awesome/stakebird/simapp"
+	"github.com/public-awesome/stakebird/x/curating"
+	curatingtypes "github.com/public-awesome/stakebird/x/curating/types"
+	"github.com/stretchr/testify/require"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+)
+
+// 1 vote  = 1 vote credit
+// 2 votes = 4 vote credits
+// 3 votes = 9 vote credits
+// 4 votes = 16 vote credits
+// 5 votes = 25 vote credits
+func TestQVF(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	// add funds to reward pool
+	funds := sdk.NewInt64Coin("ustb", 10_000_000)
+	err := app.BankKeeper.MintCoins(ctx, curatingtypes.RewardPoolName, sdk.NewCoins(funds))
+	require.NoError(t, err)
+
+	q := curating.NewQVFData(ctx, app.CuratingKeeper)
+	q, err = q.TallyVote(sdk.NewInt(1))
+	require.NoError(t, err)
+	q, err = q.TallyVote(sdk.NewInt(9))
+	require.NoError(t, err)
+
+	// for reference...
+	// require.Equal(t, int64(2), q.VoterCount)
+	// require.Equal(t, "10", q.VotingPool.String())
+	// require.Equal(t, "4.000000000000000000", q.RootSum.String())
+	require.Equal(t, "6.000000000000000000", q.MatchPool().String())
+	require.Equal(t, "5", q.VoterReward().String())
+	require.Equal(t, "3.000000000000000000", q.MatchReward().String())
+}
+
+func TestQVFZeroVotes(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	// add funds to reward pool
+	funds := sdk.NewInt64Coin("ustb", 10_000_000)
+	err := app.BankKeeper.MintCoins(ctx, curatingtypes.RewardPoolName, sdk.NewCoins(funds))
+	require.NoError(t, err)
+
+	q := curating.NewQVFData(ctx, app.CuratingKeeper)
+
+	// for reference...
+	// require.Equal(t, int64(0), q.VoterCount)
+	// require.Equal(t, "0", q.VotingPool.String())
+	// require.Equal(t, "0.000000000000000000", q.RootSum.String())
+	require.Equal(t, "0.000000000000000000", q.MatchPool().String())
+	require.Equal(t, "0", q.VoterReward().String())
+	require.Equal(t, "0.000000000000000000", q.MatchReward().String())
+}
