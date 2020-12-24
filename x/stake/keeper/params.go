0199d0005c841d5511efa0a62cd721a07c8f9c52
Shane Vitarana <shanev@gmail.com>
2020-12-23 22:37:48 -0500

    remove commented code

--- x/stake/keeper/params.go
+++ x/stake/keeper/params.go
@@ -8,18 +8,13 @@ import (
 
 // ParamKeyTable for staking module
 func ParamKeyTable() paramtypes.KeyTable {
-	// return paramtypes.NewKeyTable().RegisterParamSet(&types.Params{})
 	return paramtypes.KeyTable{}
 }
 
 // GetParams returns the total set of curating parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
-	// k.paramstore.GetParamSet(ctx, &params)
-	// return params
 	return types.Params{}
 }
 
 // SetParams sets the curating parameters to the param space.
-func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
-	// k.paramstore.SetParamSet(ctx, &params)
-}
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {}
