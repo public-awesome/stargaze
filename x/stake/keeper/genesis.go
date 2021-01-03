8c7f8448bc4d1b4732eab594fadceccaa7e1cd40
Shane Vitarana <shanev@gmail.com>
2021-01-03 15:18:08 -0500

    add todos

--- x/stake/keeper/genesis.go
+++ x/stake/keeper/genesis.go
@@ -7,9 +7,14 @@ import (
 
 // InitGenesis initializes the curating module state
 func (k Keeper) InitGenesis(ctx sdk.Context, state types.GenesisState) {
+	// TODO
+	// set stakes
 }
 
 // ExportGenesis exports the curating module state
 func (k Keeper) ExportGenesis(ctx sdk.Context) *types.GenesisState {
 	return &types.GenesisState{}
+
+	// TODO
+	// append stakes
 }
