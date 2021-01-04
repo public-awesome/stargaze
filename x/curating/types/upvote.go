16b0db14b4c7b625fb44d7a1f8737cba1804812c
Shane Vitarana <shanev@gmail.com>
2021-01-04 00:53:55 -0500

    upvote marshal to json

--- x/curating/types/upvote.go
+++ x/curating/types/upvote.go
@@ -1,8 +1,10 @@
 package types
 
 import (
+	"encoding/json"
 	"time"
 
+	"github.com/bwmarrin/snowflake"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -23,3 +25,33 @@ func NewUpvote(
 
 // Upvotes is a collection of Upvote objects
 type Upvotes []Upvote
+
+type upvotePretty struct {
+	VendorID      uint32   `json:"vendor_id" yaml:"vendor_id"`
+	PostID        string   `json:"post_id" yaml:"post_id"`
+	Curator       string   `json:"curator" yaml:"curator"`
+	RewardAccount string   `json:"reward_account" yaml:"reward_account"`
+	VoteAmount    sdk.Coin `json:"vote_amount" yaml:"vote_amount"`
+	CuratedTime   string   `json:"curated_time" yaml:"curated_time"`
+}
+
+// MarshalJSON defines custom encoding scheme
+func (u Upvote) MarshalJSON() ([]byte, error) {
+	var temp [8]byte
+	copy(temp[:], u.PostID) // convert a postID byte slice into a fixed 8 byte array
+	postID := snowflake.ParseIntBytes(temp)
+
+	out, err := json.Marshal(upvotePretty{
+		VendorID:      u.VendorID,
+		PostID:        postID.String(),
+		Curator:       u.Curator,
+		RewardAccount: u.RewardAccount,
+		VoteAmount:    u.VoteAmount,
+		CuratedTime:   u.CuratedTime.Format(time.RFC3339),
+	})
+	if err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
