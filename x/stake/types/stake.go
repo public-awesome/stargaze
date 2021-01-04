25c0d7b2baeeb6b62655ca844c2fe3523191fdb2
Shane Vitarana <shanev@gmail.com>
2021-01-04 01:08:01 -0500

    marshal stakes to json

--- x/stake/types/stake.go
+++ x/stake/types/stake.go
@@ -1,6 +1,9 @@
 package types
 
 import (
+	"encoding/json"
+
+	"github.com/bwmarrin/snowflake"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
@@ -20,3 +23,31 @@ func NewStake(
 
 // Stakes is a collection of Stake 🥩
 type Stakes []Stake
+
+type stakePretty struct {
+	VendorID  uint32  `json:"vendor_id" yaml:"vendor_id"`
+	PostID    string  `json:"post_id" yaml:"post_id"`
+	Delegator string  `json:"delegator" yaml:"delegator"`
+	Validator string  `json:"validator" yaml:"validator"`
+	Amount    sdk.Int `json:"amount" yaml:"amount"`
+}
+
+// MarshalJSON defines custom encoding scheme
+func (s Stake) MarshalJSON() ([]byte, error) {
+	var temp [8]byte
+	copy(temp[:], s.PostID) // convert a postID byte slice into a fixed 8 byte array
+	postID := snowflake.ParseIntBytes(temp)
+
+	out, err := json.Marshal(stakePretty{
+		VendorID:  s.VendorID,
+		PostID:    postID.String(),
+		Delegator: s.Delegator,
+		Validator: s.Validator,
+		Amount:    s.Amount,
+	})
+	if err != nil {
+		return out, err
+	}
+
+	return out, nil
+}
