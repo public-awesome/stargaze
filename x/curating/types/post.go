efb80b061ce6646abc820657a2196aa1c5303952
Shane Vitarana <shanev@gmail.com>
2021-01-05 12:19:00 -0500

    postid custom gogo proto type

--- x/curating/types/post.go
+++ x/curating/types/post.go
@@ -3,78 +3,42 @@ package types
 import (
 	"time"
 
+	"github.com/bwmarrin/snowflake"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// // Marshal implements the gogo proto custom type interface.
-// func (i Int) Marshal() ([]byte, error) {
-// 	if i.i == nil {
-// 		i.i = new(big.Int)
-// 	}
-// 	return i.i.MarshalText()
-// }
+// CustomProtobufType defines the interface custom gogo proto types must implement
+// in order to be used as a "customtype" extension.
+//
+// ref: https://github.com/gogo/protobuf/blob/master/custom_types.md
+type CustomProtobufType interface {
+	Marshal() ([]byte, error)
+	MarshalTo(data []byte) (n int, err error)
+	Unmarshal(data []byte) error
+	Size() int
 
-// // MarshalTo implements the gogo proto custom type interface.
-// func (i *Int) MarshalTo(data []byte) (n int, err error) {
-// 	if i.i == nil {
-// 		i.i = new(big.Int)
-// 	}
-// 	if len(i.i.Bytes()) == 0 {
-// 		copy(data, []byte{0x30})
-// 		return 1, nil
-// 	}
+	MarshalJSON() ([]byte, error)
+	UnmarshalJSON(data []byte) error
+}
 
-// 	bz, err := i.Marshal()
-// 	if err != nil {
-// 		return 0, err
-// 	}
+var _ CustomProtobufType = (*PostID)(nil)
 
-// 	copy(data, bz)
-// 	return len(bz), nil
-// }
-
-// // Unmarshal implements the gogo proto custom type interface.
-// func (i *Int) Unmarshal(data []byte) error {
-// 	if len(data) == 0 {
-// 		i = nil
-// 		return nil
-// 	}
-
-// 	if i.i == nil {
-// 		i.i = new(big.Int)
-// 	}
-
-// 	if err := i.i.UnmarshalText(data); err != nil {
-// 		return err
-// 	}
-
-// 	if i.i.BitLen() > maxBitLen {
-// 		return fmt.Errorf("integer out of range; got: %d, max: %d", i.i.BitLen(), maxBitLen)
-// 	}
-
-// 	return nil
-// }
-
-// // Size implements the gogo proto custom type interface.
-// func (i *Int) Size() int {
-// 	bz, _ := i.Marshal()
-// 	return len(bz)
-// }
-
-// type PostID []byte
-
-// PostID is a byte array
+// PostID represents a Twitter post ID (for now)
 type PostID struct {
-	id []byte
+	id snowflake.ID
 }
 
 // Marshal implements the gogo proto custom type interface.
 func (p PostID) Marshal() ([]byte, error) {
-	if p.id == nil {
-		p.id = []byte{}
-	}
+	return p.id.Bytes(), nil
+}
 
-	return p.id, nil
+func (p PostID) MarshalTo(data []byte) (n int, err error) {
+	return 8, nil
+}
+
+func (p PostID) Unmarshal(data []byte) error {
+	return nil
 }
 
 // Size implements the gogo proto custom type interface.
@@ -83,19 +47,13 @@ func (p *PostID) Size() int {
 	return len(bz)
 }
 
-// MarshalJSON encodes a post id snowflake as JSON bytes
-// func (p PostID) MarshalJSON() ([]byte, error) {
-// 	var temp [8]byte
-// 	copy(temp[:], p)
-// 	id := snowflake.ParseIntBytes(temp)
+func (p PostID) MarshalJSON() ([]byte, error) {
+	return p.MarshalJSON()
+}
 
-// 	out, err := json.Marshal(id.String())
-// 	if err != nil {
-// 		return out, err
-// 	}
-
-// 	return out, nil
-// }
+func (p PostID) UnmarshalJSON(data []byte) error {
+	return p.UnmarshalJSON(data)
+}
 
 // Posts is a collection of Post objects
 type Posts []Post
@@ -110,94 +68,10 @@ func NewPost(
 
 	return Post{
 		VendorID:        vendorID,
-		PostID:          &postIDBz,
+		PostID:          postIDBz,
 		Creator:         creator.String(),
 		RewardAccount:   rewardAccount.String(),
 		BodyHash:        bodyHash,
 		CuratingEndTime: curatingEndTime,
 	}
 }
-
-// postPretty is a representation of `Post` suitable for silly hoomans
-// type postPretty struct {
-// 	VendorID        uint32 `json:"vendor_id" yaml:"vendor_id"`
-// 	PostID          string `json:"post_id" yaml:"post_id"`
-// 	Creator         string `json:"creator" yaml:"creator"`
-// 	RewardAccount   string `json:"reward_account" yaml:"reward_account"`
-// 	BodyHash        string `json:"body_hash" yaml:"body_hash"`
-// 	CuratingEndTime string `json:"curating_end_time" yaml:"curating_end_time"`
-// }
-
-// // MarshalJSON defines custom encoding scheme
-// func (p Post) MarshalJSON() ([]byte, error) {
-// 	var temp [8]byte
-// 	copy(temp[:], p.PostID) // convert a postID byte slice into a fixed 8 byte array
-// 	postID := snowflake.ParseIntBytes(temp)
-
-// 	out, err := json.Marshal(postPretty{
-// 		VendorID:        p.VendorID,
-// 		PostID:          postID.String(),
-// 		Creator:         p.Creator,
-// 		RewardAccount:   p.RewardAccount,
-// 		BodyHash:        hex.EncodeToString(p.BodyHash),
-// 		CuratingEndTime: p.CuratingEndTime.Format(time.RFC3339),
-// 	})
-// 	if err != nil {
-// 		return out, err
-// 	}
-
-// 	return out, nil
-// }
-
-// func (vo *VoteOption) UnmarshalJSON(data []byte) error {
-// 	var s string
-// 	err := json.Unmarshal(data, &s)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	bz2, err := VoteOptionFromString(s)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	*vo = bz2
-// 	return nil
-// }
-
-// UnmarshalJSON decodes JSON bytes into a Post
-// func (p *Post) UnmarshalJSON(data []byte) error {
-// 	// TODO: do a custom unmarshaller for PostID only?
-
-// 	return nil
-// }
-
-// PostIDStr returns a string representation of the underlying bytes that conforms an id.
-// func (p Post) PostIDStr() string {
-// 	var temp [8]byte
-// 	copy(temp[:], p.PostID) // convert a postID byte slice into a fixed 8 byte array
-// 	postID := snowflake.ParseIntBytes(temp)
-// 	return postID.String()
-// }
-
-// type vpPairPretty struct {
-// 	VendorID uint32 `json:"vendor_id" yaml:"vendor_id"`
-// 	PostID   string `json:"post_id" yaml:"post_id"`
-// }
-
-// // MarshalJSON defines custom encoding scheme
-// func (vp VPPair) MarshalJSON() ([]byte, error) {
-// 	var temp [8]byte
-// 	copy(temp[:], vp.PostID) // convert a postID byte slice into a fixed 8 byte array
-// 	postID := snowflake.ParseIntBytes(temp)
-
-// 	out, err := json.Marshal(vpPairPretty{
-// 		VendorID: vp.VendorID,
-// 		PostID:   postID.String(),
-// 	})
-// 	if err != nil {
-// 		return out, err
-// 	}
-
-// 	return out, nil
-// }
