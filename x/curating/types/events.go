86b8332665bec6c32ed7e5c5dc4d4193825ba2d6
Shane Vitarana <shanev@gmail.com>
2021-01-14 23:33:06 -0500

    removed unused events

--- x/curating/types/events.go
+++ x/curating/types/events.go
@@ -4,27 +4,20 @@ package types
 const (
 	EventTypePost             = "post"
 	EventTypeUpvote           = "upvote"
-	EventTypeModerate         = "moderate"
 	EventTypeCurationComplete = "curation_complete"
 	EventTypeProtocolReward   = "protocol_reward"
-	EventTypeVotingPoolReturn = "voting_pool_return"
 
 	AttributeKeyVendorID           = "vendor_id"
 	AttributeKeyPostID             = "post_id"
 	AttributeKeyCreator            = "creator"
 	AttributeKeyCurator            = "curator"
-	AttributeKeyModerator          = "moderator"
 	AttributeKeyRewardAccount      = "reward_account"
-	AttributeKeyDeposit            = "deposit"
 	AttributeKeyBody               = "body"
 	AttributeCurationEndTime       = "curation_end_time"
 	AttributeKeyVoteNumber         = "vote_number"
 	AttributeKeyVoteAmount         = "vote_amount"
 	AttributeKeyVoteDenom          = "vote_denom"
 	AttributeKeyProtocolRewardType = "reward_type"
-
-	AttributeRewardTypeCreator = "creator"
-	AttributeRewardTypeCurator = "curator"
 
 	AttributeValueCategory = ModuleName
 )
