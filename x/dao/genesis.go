d24e619bf1c7819f296d2005c121b15bb3654a65
Shane Vitarana <shanev@gmail.com>
2021-05-23 19:57:58 -0400

    Re-generated proto with stringer

--- x/dao/genesis.go
+++ x/dao/genesis.go
@@ -9,8 +9,8 @@ import (
 // InitGenesis initializes the dao module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, dk types.DistKeeper, genState types.GenesisState) {
-	amount := sdk.NewCoins(sdk.NewInt64Coin("ustarx", 5_000_000_000))
-	sender, err := sdk.AccAddressFromBech32("stars1czlu4tvr3dg3ksuf8zak87eafztr2u004zyh5a")
+	amount := sdk.NewCoins(genState.Params.GenesisAllocation)
+	sender, err := sdk.AccAddressFromBech32(genState.Params.GenesisSender)
 	if err != nil {
 		panic(err)
 	}
