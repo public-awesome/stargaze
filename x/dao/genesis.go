abcdc4abf430206a31456cff4fb42a63d79dfdcc
Shane Vitarana <shanev@gmail.com>
2021-05-23 18:04:21 -0400

    Moved init genesis into scaffolded file

--- x/dao/genesis.go
+++ x/dao/genesis.go
@@ -9,18 +9,26 @@ import (
 // InitGenesis initializes the capability module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
-    // this line is used by starport scaffolding # genesis/module/init
+	amount := sdk.NewCoins(sdk.NewInt64Coin("ustarx", 5_000_000_000))
+	sender, err := sdk.AccAddressFromBech32("stars1czlu4tvr3dg3ksuf8zak87eafztr2u004zyh5a")
+	if err != nil {
+		panic(err)
+	}
 
-    // this line is used by starport scaffolding # ibc/genesis/init
+	k.distKeeper.FundCommunityPool(ctx, amount, sender)
+
+	// this line is used by starport scaffolding # genesis/module/init
+
+	// this line is used by starport scaffolding # ibc/genesis/init
 }
 
 // ExportGenesis returns the capability module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
 	genesis := types.DefaultGenesis()
 
-    // this line is used by starport scaffolding # genesis/module/export
+	// this line is used by starport scaffolding # genesis/module/export
 
-    // this line is used by starport scaffolding # ibc/genesis/export
+	// this line is used by starport scaffolding # ibc/genesis/export
 
-    return genesis
+	return genesis
 }
