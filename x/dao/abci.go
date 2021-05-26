f7c1e5e72a6905ed6583bdba3c27977b4e876482
Shane Vitarana <shanev@gmail.com>
2021-05-25 21:01:48 -0400

    Add ABCI end blocker function

---
+++ x/dao/abci.go
@@ -0,0 +1,32 @@
+package dao
+
+import (
+	"time"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	"github.com/cosmos/cosmos-sdk/telemetry"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/public-awesome/stargaze/x/dao/keeper"
+	"github.com/public-awesome/stargaze/x/dao/types"
+)
+
+// EndBlocker called every block, update validator set
+func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
+	defer telemetry.ModuleMeasureSince(types.ModuleName, time.Now(), telemetry.MetricKeyEndBlocker)
+
+	// funder, err := sdk.AccAddressFromBech32(genState.Params.Funder)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// daoFund := bk.GetAllBalances(ctx, funder)
+
+	// if !daoFund.Empty() {
+	// 	err = dk.FundCommunityPool(ctx, daoFund, funder)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
+	return []abci.ValidatorUpdate{}
+}
