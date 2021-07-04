59ee9aedde6fd065b309673654632de14c22b52c
Shane Vitarana <shanev@gmail.com>
2021-07-03 23:48:52 -0400

    Upload script

--- scripts/wasm/upload.sh
+++ scripts/wasm/upload.sh
@@ -8,9 +8,7 @@ starsd config keyring-backend test
 starsd q wasm list-code
 
 # download cw20-bonding contract code
-curl -O https://github.com/CosmWasm/cosmwasm-plus/releases/download/v0.6.2/cw20_bonding.wasm
+curl -LO https://github.com/CosmWasm/cosmwasm-plus/releases/download/v0.6.2/cw20_bonding.wasm
 
-RES=$(starsd tx wasm store cw20_bonding.wasm --from user1 -y)
-
-CODE_ID=$(echo $RES | jq -r '.logs[0].events[0].attributes[-1].value')
-echo "uploaded code id: $CODE_ID"
+# upload contract code
+starsd tx wasm store cw20_bonding.wasm --from user1 --gas-prices 0.01ustarx --gas auto --gas-adjustment 1.3 -y -b block
