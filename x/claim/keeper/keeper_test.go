a4d52153914297905237f33f00faeb218a5f8a04
Shane Vitarana <shanev@gmail.com>
2021-07-10 14:32:48 -0400

    Update claim CLI

--- x/claim/keeper/keeper_test.go
+++ x/claim/keeper/keeper_test.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/osmosis-labs/osmosis/app"
+	"github.com/public-awesome/stargaze/app"
 	"github.com/public-awesome/stargaze/x/claim/types"
 	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
