b733c0653acd5a2bec177cb917ddaa454379c1a3
jhernandezb <contact@jhernandez.me>
2021-08-14 09:42:23 -0600

    increase timeout

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -4,5 +4,5 @@ cp ./scripts/ci/hermes/config.toml ~/.hermes/
 cat $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
-sleep 75
+sleep 300
 hermes create channel stargaze gaia --port-a transfer --port-b transfer
