6747431209f3295574fad3b736849b9eb6f88dbd
jhernandezb <contact@jhernandez.me>
2021-08-14 09:34:49 -0600

    fix paths

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -1,6 +1,7 @@
 set -ex
 mkdir -p ~/.hermes/
 cp ./scripts/ci/hermes/config.toml ~/.hermes/
-hermes keys add stargaze -f ./scripts/ci/hermes/stargaze.json
-hermes keys add gaia -f ./scripts/ci/hermes/gaia.json
+cat $PWD/scripts/ci/hermes/stargaze.json
+hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
+hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
 hermes create channel stargaze gaia --port-a transfer --port-b transfer
