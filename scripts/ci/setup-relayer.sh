92fa6bf5c18dd240086d7aac4fb52c47498fbc20
jhernandezb <contact@jhernandez.me>
2021-08-14 10:27:52 -0600

    remove check

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -5,6 +5,4 @@ cat $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
 sleep 360
-curl -s http://gaia:26657/status
-curl -s http://stargaze:26657/status
 hermes create channel stargaze gaia --port-a transfer --port-b transfer
