d816c0b68ea42850d63af5737545cd3b8c11acdc
jhernandezb <contact@jhernandez.me>
2021-08-14 09:40:05 -0600

    wait for readiness

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -4,4 +4,5 @@ cp ./scripts/ci/hermes/config.toml ~/.hermes/
 cat $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
+sleep 75
 hermes create channel stargaze gaia --port-a transfer --port-b transfer
