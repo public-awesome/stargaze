b118e10c1c365b3187f7f8027edb5b2a8b5dfea0
jhernandezb <contact@jhernandez.me>
2021-08-15 20:34:59 -0600

    propery fix

--- scripts/ci/setup-gaia.sh
+++ scripts/ci/setup-gaia.sh
@@ -9,7 +9,7 @@ gaiad init --chain-id $CHAINID $CHAINID
 sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' ~/.gaia/config/config.toml
 sed -i "s/\"stake\"/\"$DENOM\"/g" ~/.gaia/config/genesis.json
 sed -i 's/pruning = "syncable"/pruning = "nothing"/g' ~/.gaia/config/app.toml
-sed -i 's/enabled = false/enabled = true/g' ~/.gaia/config/app.toml
+sed -i 's/enable = false/enable = true/g' ~/.gaia/config/app.toml
 gaiad keys --keyring-backend test add validator
 
 gaiad add-genesis-account $(gaiad keys --keyring-backend test show validator -a) 100000000000$DENOM
