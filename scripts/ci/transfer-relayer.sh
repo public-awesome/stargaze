14f194e554cfc78ec4ec8c25dc721d057ef78ef4
jhernandezb <contact@jhernandez.me>
2021-08-15 18:39:16 -0600

    wait for relayer

--- scripts/ci/transfer-relayer.sh
+++ scripts/ci/transfer-relayer.sh
@@ -4,3 +4,4 @@ cp ./scripts/ci/hermes/config.toml ~/.hermes/
 hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
 hermes tx raw ft-transfer stargaze gaia transfer channel-0 9999 -d stake -o 1000 -n 2
+sleep 30
