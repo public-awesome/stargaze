315fc67e9ffb5449ad84bb2e133f944d8d230e38
jhernandezb <contact@jhernandez.me>
2021-08-15 20:04:56 -0600

    check balances

--- scripts/ci/transfer-relayer.sh
+++ scripts/ci/transfer-relayer.sh
@@ -6,3 +6,7 @@ hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
 hermes tx raw ft-transfer stargaze gaia transfer channel-0 9999 -d stake -o 1000 -n 2
 hermes tx raw ft-transfer gaia stargaze transfer channel-0 9999 -d ustarx -o 1000 -n 2
 sleep 30
+export GAIA_ADDRESS=cosmos1wt3khka7cmn5zd592x430ph4zmlhf5gfztgha6
+export STARGAZE_ADDRESS=stars12g0xe2ld0k5ws3h7lmxc39d4rpl3fyxp5qys69
+curl -s http://gaia:1317/bank/balances/$GAIA_ADDRESS | jq '.'
+curl -s http://stargaze:1317/bank/balances/$STARGAZE_ADDRESS | jq '.'
