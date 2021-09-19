ffd8f145c7fda9e2d0f37b54ce00b93619278787
Shane Vitarana <shanev@gmail.com>
2021-09-19 17:48:43 -0400

    Mint more tokens

--- scripts/wasm/cw721-base.sh
+++ scripts/wasm/cw721-base.sh
@@ -30,8 +30,16 @@ starsd q wasm contract-state smart $CONTRACT '{"num_tokens":{}}'
 starsd q wasm contract-state smart $CONTRACT '{"contract_info":{}}'
 
 # execute a mint operation
-MINT="{\"mint\":{\"token_id\":\"1\",\"owner\":\"$INVESTOR\",\"name\":\"bobo\",\"image\":\"https://lh3.googleusercontent.com/Xv5i9YaUJO73claDwpJ-cqkx6-xhrrZnF9QAD_Qn4aPJHSxklQp4anlJuV-GABs4ZkB1lIsmkodUT3V8ER0wivNrwBswUHZzrpVOaQ=h1328\"}}"
-starsd tx wasm execute $CONTRACT $MINT --from creator $TXFLAG
+MINT1="{\"mint\":{\"token_id\":\"1\",\"owner\":\"$INVESTOR\",\"name\":\"bobo1\",\"description\":\"Ape556\",\"image\":\"https://lh3.googleusercontent.com/Xv5i9YaUJO73claDwpJ-cqkx6-xhrrZnF9QAD_Qn4aPJHSxklQp4anlJuV-GABs4ZkB1lIsmkodUT3V8ER0wivNrwBswUHZzrpVOaQ=h1328\"}}"
+MINT2="{\"mint\":{\"token_id\":\"2\",\"owner\":\"$INVESTOR\",\"name\":\"bobo2\",\"description\":\"Ape557\",\"image\":\"https://images.squarespace-cdn.com/content/v1/5c12933f365f02733c923e4e/1623457826739-RFS8YBP06I1W5WW2CSCG/tyler-hobbs-fidenza-612.png?format=750w\"}}"
+MINT3="{\"mint\":{\"token_id\":\"3\",\"owner\":\"$INVESTOR\",\"name\":\"bobo3\",\"description\":\"Ape558\",\"image\":\"https://miro.medium.com/max/1400/1*Fg9-AmnE5X_929CqE1xp9w.png\"}}"
+MINT4="{\"mint\":{\"token_id\":\"4\",\"owner\":\"$INVESTOR\",\"name\":\"bobo4\",\"description\":\"Ape559\",\"image\":\"https://lh3.googleusercontent.com/Zau-70Ga57u021g4xxx9UqHyiwwpxuFI-W1q0BWetxhmhm8_rTERCPsCfQled_nxBDIN40U7x1hDX3CvVkMeLe4Pxg=w600\"}}"
+MINT5="{\"mint\":{\"token_id\":\"5\",\"owner\":\"$INVESTOR\",\"name\":\"bobo5\",\"description\":\"Ape554\",\"image\":\"https://lh3.googleusercontent.com/sjXMZ1J39uU7UlpXjm8TmGclwNbC6YiITFZXAmj0brUE9Yw_cLKeyFPTMRur2etRGfifYdIFdDpN7qZ9Vn9I3GTfUTP8DX0S05mv8w=w600\"}}"
+starsd tx wasm execute $CONTRACT $MINT1 --from creator $TXFLAG
+starsd tx wasm execute $CONTRACT $MINT2 --from creator $TXFLAG
+starsd tx wasm execute $CONTRACT $MINT3 --from creator $TXFLAG
+starsd tx wasm execute $CONTRACT $MINT4 --from creator $TXFLAG
+starsd tx wasm execute $CONTRACT $MINT5 --from creator $TXFLAG
 
 # query contract
 starsd q wasm contract-state smart $CONTRACT '{"owner_of":{"token_id":"1"}}'
