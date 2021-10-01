6a9340bba64f35bf05325e1a358549e0f40e8a94
jhernandezb <contact@jhernandez.me>
2021-09-30 23:16:30 -0600

    cleanup

--- scripts/go-test.sh
+++ scripts/go-test.sh
@@ -1,10 +1,7 @@
-
 #!/usr/bin/env bash
 
-echo "go path $GOPATH"
 set -e
 echo "" > coverage.txt
-go mod download
 for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic "$d"
     if [ -f profile.out ]; then
