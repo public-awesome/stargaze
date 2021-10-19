54b258073ac2582c8bc24eae4fe8a7db59bc8b2d
Shane Vitarana <shanev@gmail.com>
2021-10-19 17:15:28 -0400

    Updated root cmd

--- cmd/starsd/main.go
+++ cmd/starsd/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"os"
 
+	"github.com/cosmos/cosmos-sdk/server"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/cosmos/cosmos-sdk/simapp/simd/cmd"
 	"github.com/public-awesome/stargaze/app"
 	"github.com/public-awesome/stargaze/cmd/starsd/cmd"
 	"github.com/tendermint/spm/cosmoscmd"
@@ -21,6 +23,12 @@ func main() {
 		// this line is used by starport scaffolding # root/arguments
 	)
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
-		os.Exit(1)
+		switch e := err.(type) {
+		case server.ErrorCode:
+			os.Exit(e.Code)
+
+		default:
+			os.Exit(1)
+		}
 	}
 }
