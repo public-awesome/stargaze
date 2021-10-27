40877055a5e24facf2e2245c42fef0dd1cec9ce8
jhernandezb <contact@jhernandez.me>
2021-10-26 23:15:16 -0600

    fund community pool on genesis

--- x/alloc/genesis.go
+++ x/alloc/genesis.go
@@ -10,6 +10,10 @@ import (
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
 	k.SetParams(ctx, genState.Params)
+	err := k.FundCommunityPool(ctx)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // ExportGenesis returns the capability module's exported genesis.
