5a7b5267981dcc215dca73675d063568c53c2bcd
jhernandezb <contact@jhernandez.me>
2022-01-02 20:49:05 -0600

    fix test keys

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -3,5 +3,6 @@ mkdir -p ~/.hermes/
 cp ./scripts/ci/hermes/config.toml ~/.hermes/
 hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
+hermes keys add osmosis -f $PWD/scripts/ci/hermes/osmosis.json
 hermes create channel stargaze gaia --port-a transfer --port-b transfer
 hermes create channel stargaze osmosis --port-a transfer --port-b transfer
