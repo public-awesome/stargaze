9990e047205cbd48a6ae1b81875fb8d631c36030
jhernandezb <contact@jhernandez.me>
2022-01-05 11:44:48 -0600

    Update CHANGELOG

--- x/claim/module.go
+++ x/claim/module.go
@@ -1,6 +1,7 @@
 package claim
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
@@ -78,7 +79,10 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
-	// this line is used by starport scaffolding # 2
+	err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx))
+	if err != nil {
+		panic(err)
+	}
 }
 
 // GetTxCmd returns the capability module's root tx command.
