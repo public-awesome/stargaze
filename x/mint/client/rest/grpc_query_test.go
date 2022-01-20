d327a71cbf1dfd354c266f2fa7b8222508135bb4
jhernandezb <contact@jhernandez.me>
2022-01-19 18:04:23 -0600

    fix import

--- x/mint/client/rest/grpc_query_test.go
+++ x/mint/client/rest/grpc_query_test.go
@@ -1,3 +1,4 @@
+//go:build norace
 // +build norace
 
 package rest_test
@@ -14,7 +15,7 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	"github.com/cosmos/cosmos-sdk/testutil/network"
-	minttypes "github.com/public-awesome/stargaze/x/mint/types"
+	minttypes "github.com/public-awesome/stargaze/v2/x/mint/types"
 )
 
 type IntegrationTestSuite struct {
