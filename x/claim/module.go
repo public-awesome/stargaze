5fe1eb62af2d216d90ee4eafb3179ce677ba291e
jhernandezb <contact@jhernandez.me>
2022-01-20 18:20:55 -0600

    register no-op migrations for alloc,claim

--- x/claim/module.go
+++ x/claim/module.go
@@ -135,6 +135,7 @@ func (am AppModule) LegacyQuerierHandler(legacyQuerierCdc *codec.LegacyAmino) sd
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	cfg.RegisterMigration(types.ModuleName, 2, func(sdk.Context) error { return nil })
 }
 
 // RegisterInvariants registers the capability module's invariants.
