0027ed8d68691db0367add14910ae890b777cce6
jhernandezb <contact@jhernandez.me>
2022-02-16 11:49:51 -0600

    handle claim for message

--- x/claim/keeper/msg_server_claim_for.go
+++ x/claim/keeper/msg_server_claim_for.go
@@ -9,9 +9,41 @@ import (
 
 func (k msgServer) ClaimFor(goCtx context.Context, msg *types.MsgClaimFor) (*types.MsgClaimForResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
-
-	// TODO: Handling the message
-	_ = ctx
-
-	return &types.MsgClaimForResponse{}, nil
+	_, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return nil, err
+	}
+	address, err := sdk.AccAddressFromBech32(msg.Address)
+	if err != nil {
+		return nil, err
+	}
+	params := k.GetParams(ctx)
+	if !params.IsAirdropEnabled(ctx.BlockTime()) {
+		return nil, types.ErrAirdropNotEnabled
+	}
+	allowed := false
+	for _, authorization := range params.AllowedClaimers {
+		if authorization.ContractAddress == msg.Sender && authorization.Action == msg.Action {
+			allowed = true
+			break
+		}
+	}
+	if !allowed {
+		return nil, types.ErrUnauthorizedClaimer
+	}
+	coins, err := k.Keeper.ClaimCoinsForAction(ctx, address, types.ActionInitialClaim)
+	if err != nil {
+		return nil, err
+	}
+	ctx.EventManager().EmitEvents(sdk.Events{
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, types.AttributeValueCategory),
+			sdk.NewAttribute(sdk.AttributeKeySender, msg.Sender),
+		),
+	})
+	return &types.MsgClaimForResponse{
+		Address:       msg.Address,
+		ClaimedAmount: coins,
+	}, nil
 }
