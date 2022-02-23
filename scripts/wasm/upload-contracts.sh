fcac9d05c33451b88f09b7b680a923537fbbb4a3
Shane Vitarana <shanev@gmail.com>
2022-02-22 19:44:58 -0500

    Update to work with new repos

--- scripts/wasm/upload-contracts.sh
+++ scripts/wasm/upload-contracts.sh
@@ -14,8 +14,10 @@ fi
 
 TXFLAG="--gas-prices 0.01$DENOM --gas auto --gas-adjustment 1.3 -y -b block --chain-id $CHAIN_ID --node $NODE --output json"
 
-REPO=https://github.com/public-awesome/contracts
-TAG=v0.1.9
+CONTRACTS_REPO=https://github.com/public-awesome/stargaze-contracts
+CONTRACTS_TAG=v0.1.10
+MARKETPLACE_REPO=https://github.com/public-awesome/marketplace
+MARKETPLACE_TAG=v0.1.12
 
 if [[ -z "$GITHUB_OAUTH_TOKEN" ]]; then
     echo "Must set GITHUB_OAUTH_TOKEN in environment" 1>&2
@@ -29,12 +31,12 @@ then
 fi
 
 # Fetch contract wasm binaries
-fetch --repo=$REPO --tag=$TAG --release-asset="collection_factory.wasm" .
-fetch --repo=$REPO --tag=$TAG --release-asset="sg_marketplace.wasm" .
-fetch --repo=$REPO --tag=$TAG --release-asset="sg721.wasm" .
-fetch --repo=$REPO --tag=$TAG --release-asset="minter.wasm" .
-fetch --repo=$REPO --tag=$TAG --release-asset="whitelist.wasm" .
-fetch --repo=$REPO --tag=$TAG --release-asset="royalty_group.wasm" .
+fetch --repo=$MARKETPLACE_REPO --tag=$TAG --release-asset="collection_factory.wasm" .
+fetch --repo=$MARKETPLACE_REPO --tag=$TAG --release-asset="sg_marketplace.wasm" .
+fetch --repo=$CONTRACTS_REPO --tag=$TAG --release-asset="sg721.wasm" .
+fetch --repo=$CONTRACTS_REPO --tag=$TAG --release-asset="minter.wasm" .
+fetch --repo=$CONTRACTS_REPO --tag=$TAG --release-asset="whitelist.wasm" .
+fetch --repo=$CONTRACTS_REPO --tag=$TAG --release-asset="royalty_group.wasm" .
 fetch --repo=https://github.com/CosmWasm/cw-nfts --tag=v0.11.0 --release-asset="cw721_metadata_onchain.wasm" .
 fetch --repo=https://github.com/CosmWasm/cw-plus --tag=v0.11.1 --release-asset="cw4_group.wasm" .
 
