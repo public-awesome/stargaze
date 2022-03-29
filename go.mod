3908e6b659e40f9dc4dfb3759b395248880aeb1f
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2022-03-29 10:53:20 -0600

    bump wasmd (#571)
    * bump wasmd
    
    * switch to exported method

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/public-awesome/stargaze/v3
 go 1.17
 
 require (
-	github.com/CosmWasm/wasmd v0.23.0
-	github.com/CosmWasm/wasmvm v1.0.0-beta5
+	github.com/CosmWasm/wasmd v0.24.0
+	github.com/CosmWasm/wasmvm v1.0.0-beta7
 	github.com/armon/go-metrics v0.3.10
 	github.com/cosmos/cosmos-sdk v0.45.1
 	github.com/cosmos/go-bip39 v1.0.0
@@ -124,7 +124,6 @@ require (
 
 replace (
 	github.com/99designs/keyring => github.com/cosmos/keyring v1.1.7-0.20210622111912-ef00f8ac3d76
-	github.com/CosmWasm/wasmvm => github.com/CosmWasm/wasmvm v1.0.0-beta6
 	github.com/gin-gonic/gin => github.com/gin-gonic/gin v1.7.0
 	github.com/gogo/protobuf => github.com/regen-network/protobuf v1.3.3-alpha.regen.1
 	github.com/tendermint/spm => github.com/public-awesome/spm v0.1.9-stargaze.0.20220218155531-af173fcd55c7
--- internal/wasm/distribution.go
+++ internal/wasm/distribution.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 
+	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -21,7 +22,7 @@ type FundCommunityPool struct {
 }
 
 func (fcp FundCommunityPool) Encode(contract sdk.AccAddress) ([]sdk.Msg, error) {
-	amount, err := convertWasmCoinsToSdkCoins(fcp.Amount)
+	amount, err := wasmkeeper.ConvertWasmCoinsToSdkCoins(fcp.Amount)
 	if err != nil {
 		return nil, err
 	}
@@ -43,28 +44,4 @@ func CustomDistributionEncoder(contract sdk.AccAddress, data json.RawMessage, ve
 		return msg.FundCommunityPool.Encode(contract)
 	}
 	return nil, fmt.Errorf("wasm: invalid custom distribution message")
-}
-
-func convertWasmCoinsToSdkCoins(coins []wasmvmtypes.Coin) (sdk.Coins, error) {
-	var toSend sdk.Coins
-	for _, coin := range coins {
-		c, err := convertWasmCoinToSdkCoin(coin)
-		if err != nil {
-			return nil, err
-		}
-		toSend = append(toSend, c)
-	}
-	return toSend, nil
-}
-
-func convertWasmCoinToSdkCoin(coin wasmvmtypes.Coin) (sdk.Coin, error) {
-	amount, ok := sdk.NewIntFromString(coin.Amount)
-	if !ok {
-		return sdk.Coin{}, sdkerrors.Wrap(sdkerrors.ErrInvalidCoins, coin.Amount+coin.Denom)
-	}
-	r := sdk.Coin{
-		Denom:  coin.Denom,
-		Amount: amount,
-	}
-	return r, r.Validate()
 }
