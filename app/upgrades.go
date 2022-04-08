80cfeaa4c7123e87de162b9d8038eacb7d7cbff4
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2022-04-08 12:12:18 -0600

    v4 upgrade handler (#583)
    * add v4 upgrade handler
    
    * add upgrade to ci
    
    * update config to latest hermes
    
    * update config

--- app/upgrades.go
+++ app/upgrades.go
@@ -3,8 +3,6 @@ package app
 import (
 	"fmt"
 
-	"github.com/CosmWasm/wasmd/x/wasm"
-	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	store "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -12,27 +10,12 @@ import (
 )
 
 // next upgrade name
-const upgradeName = "v3"
+const upgradeName = "v4"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 	app.UpgradeKeeper.SetUpgradeHandler(upgradeName, func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		newVM, err := app.mm.RunMigrations(ctx, cfg, vm)
-		if err != nil {
-			return newVM, err
-		}
-		// consensus params
-		// increase max gas as part of the upgrade to handle cosmwam
-		consensusParams := app.BaseApp.GetConsensusParams(ctx)
-		consensusParams.Block.MaxGas = 75_000_000 // 75M
-		app.BaseApp.StoreConsensusParams(ctx, consensusParams)
-
-		// wasm params
-		wasmParams := app.WasmKeeper.GetParams(ctx)
-		wasmParams.CodeUploadAccess = wasmtypes.AllowNobody
-		wasmParams.MaxWasmCodeSize = DefaultMaxWasmCodeSize
-		app.WasmKeeper.SetParams(ctx, wasmParams)
-		return newVM, err
+		return app.mm.RunMigrations(ctx, cfg, vm)
 	})
 
 	upgradeInfo, err := app.UpgradeKeeper.ReadUpgradeInfoFromDisk()
@@ -42,7 +25,7 @@ func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 
 	if upgradeInfo.Name == upgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
 		storeUpgrades := store.StoreUpgrades{
-			Added: []string{wasm.ModuleName},
+			Added: []string{},
 		}
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
