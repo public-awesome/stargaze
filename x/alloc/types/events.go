25a8badee91d430564bb97aeb0c7a62f1771de4b
jhernandezb <contact@jhernandez.me>
2022-06-21 23:51:16 -0600

    fix emit event

--- x/alloc/types/events.go
+++ x/alloc/types/events.go
@@ -1,6 +1,6 @@
 package types
 
 const (
-	EventTypeClaim         = "fund_fairburn_pool"
-	AttributeValueCategory = ModuleName
+	EventTypeFundFairburnPool = "fund_fairburn_pool"
+	AttributeValueCategory    = ModuleName
 )
