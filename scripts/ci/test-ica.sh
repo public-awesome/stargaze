31f29c9bce63a4739b7794280801a6f10a76d306
jhernandezb <contact@jhernandez.me>
2022-08-26 10:46:25 -0600

    fix location

--- scripts/ci/test-ica.sh
+++ scripts/ci/test-ica.sh
@@ -1,4 +1,4 @@
-MNEMONIC=$(cat hermes/icad.json | jq -r .mnemonic)
+MNEMONIC=$(cat $PWD/scripts/ci/hermes/icad.json | jq -r .mnemonic)
 echo "$MNEMONIC" | icad keys add ica --recover --keyring-backend test
 ICA_WALLET_ADDRESS=$(icad keys show ica -a --keyring-backend test)
 icad tx intertx register --from $ICA_WALLET_ADDRESS  -connection-id connection-0 --chain-id ica --keyring-backend test
