78ee0f1dccecec4ef4115554ef58f289bbc0eb6b
jhernandezb <contact@jhernandez.me>
2022-08-25 23:27:52 -0600

    fix names

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -1,10 +1,10 @@
 set -ex
 mkdir -p ~/.hermes/
 cp ./scripts/ci/hermes/config.toml ~/.hermes/
-hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
-hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
-hermes keys add osmosis -f $PWD/scripts/ci/hermes/osmosis.json
-hermes keys add icad -f $PWD/scripts/ci/hermes/icad.json
+hermes keys add stargaze-test-1 -f $PWD/scripts/ci/hermes/stargaze.json
+hermes keys add gaia-test-1 -f $PWD/scripts/ci/hermes/gaia.json
+hermes keys add osmosis-test-1 -f $PWD/scripts/ci/hermes/osmosis.json
+hermes keys add icad-test-1 -f $PWD/scripts/ci/hermes/icad.json
 hermes create channel stargaze-test-1 gaia-test-1 --port-a transfer --port-b transfer
 hermes create channel stargaze-test-1 osmosis-test-1 --port-a transfer --port-b transfer
 hermes create channel stargaze-test-1 icad-test-1 --port-a transfer --port-b transfer
