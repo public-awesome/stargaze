fc36b0ea93df8aa4d82139276d00b524565180f2
jhernandezb <contact@jhernandez.me>
2022-08-25 23:57:21 -0600

    fix cli

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -5,6 +5,6 @@ hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
 hermes keys add osmosis -f $PWD/scripts/ci/hermes/osmosis.json
 hermes keys add icad -f $PWD/scripts/ci/hermes/icad.json
-hermes create channel stargaze gaia --port-a transfer --port-b transfer --new-client-connection
-hermes create channel stargaze osmosis --port-a transfer --port-b transfer --new-client-connection
-hermes create channel stargaze icad --port-a transfer --port-b transfer --new-client-connection
+hermes create channel stargaze connection-0 --chain-b gaia --port-a transfer --port-b transfer --new-client-connection
+hermes create channel stargaze connection-1 --chain-b osmosis --port-a transfer --port-b transfer --new-client-connection
+hermes create channel stargaze connection-2 --chain-b icad --port-a transfer --port-b transfer --new-client-connection
--- scripts/ci/setup-stargaze.sh
+++ scripts/ci/setup-stargaze.sh
@@ -13,6 +13,8 @@ sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' ~/.starsd/config/config.t
 sed -i "s/\"stake\"/\"$DENOM\"/g" ~/.starsd/config/genesis.json
 sed -i 's/pruning = "syncable"/pruning = "nothing"/g' ~/.starsd/config/app.toml
 sed -i 's/enable = false/enable = true/g' ~/.starsd/config/app.toml
+sed -i -e 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.starsd/config/config.toml
+sed -i -e 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.starsd/config/config.toml
 sed -i -e 's/\"allow_messages\":.*/\"allow_messages\": [\"\/cosmos.bank.v1beta1.MsgSend\", \"\/cosmos.staking.v1beta1.MsgDelegate\"]/g' ~/.starsd/config/genesis.json
 starsd keys --keyring-backend test add validator
 
