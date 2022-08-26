173916bda6276e0e2cb70dc8029348ab3516b673
jhernandezb <contact@jhernandez.me>
2022-08-26 11:37:39 -0600

    add new key

--- scripts/ci/setup-stargaze.sh
+++ scripts/ci/setup-stargaze.sh
@@ -18,8 +18,9 @@ sed -i -e 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.starsd/config/c
 sed -i -e 's/\"allow_messages\":.*/\"allow_messages\": [\"\/cosmos.bank.v1beta1.MsgSend\", \"\/cosmos.staking.v1beta1.MsgDelegate\"]/g' ~/.starsd/config/genesis.json
 starsd keys --keyring-backend test add validator
 
-starsd add-genesis-account $(starsd keys --keyring-backend test show validator -a) 100000000000$DENOM
-starsd add-genesis-account $RLYKEY 100000000000$DENOM
+starsd add-genesis-account $(starsd keys --keyring-backend test show validator -a) 1000000000000$DENOM
+starsd add-genesis-account $RLYKEY 1000000000000$DENOM
+starsd add-genesis-account stars1y8tcah6r989vna00ag65xcqn6mpasjjdekwfhm 1000000000000$DENOM
 starsd gentx validator 900000000$DENOM --keyring-backend test --chain-id $CHAINID
 starsd collect-gentxs
 
