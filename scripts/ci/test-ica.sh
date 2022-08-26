3c53cd83a58c5d629765e36ac4bedaf280a40c78
jhernandezb <contact@jhernandez.me>
2022-08-26 12:53:26 -0600

    test

--- scripts/ci/test-ica.sh
+++ scripts/ci/test-ica.sh
@@ -10,7 +10,10 @@ icad config node http://icad:26657
 icad status 
 icad query account $ICA_WALLET_ADDRESS --node http://icad:26657
 icad q bank balances $ICA_WALLET_ADDRESS --node http://icad:26657
-icad tx intertx register --from $ICA_WALLET_ADDRESS  --connection-id connection-0 --chain-id ica --keyring-backend test --node http://icad:26657 -b block --yes
+icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id ica --keyring-backend test  --generate-only >  tx.json  
+cat tx.json | jq
+icad tx sign tx.json --from $ICA_WALLET_ADDRESS --chain-id ica --keyring-backend test 
+icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id ica --keyring-backend test --node http://icad:26657 -b block --yes
 icad query intertx interchainaccounts connection-0 $ICA_WALLET_ADDRESS --node http://icad:26657
 export ICA_ADDR=$(icad query intertx interchainaccounts connection-0 $ICA_WALLET_ADDRESS  -o json  --node http://icad:26657 | jq -r '.interchain_account_address') && echo $ICA_ADDR
 
