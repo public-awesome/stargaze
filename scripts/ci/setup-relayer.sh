50faefb7676ed41d38cca7b6b6e53848e2913283
jhernandezb <contact@jhernandez.me>
2022-08-26 09:16:32 -0600

    only create connection

--- scripts/ci/setup-relayer.sh
+++ scripts/ci/setup-relayer.sh
@@ -7,8 +7,7 @@ hermes keys add osmosis -f $PWD/scripts/ci/hermes/osmosis.json
 hermes keys add icad -f $PWD/scripts/ci/hermes/icad.json
 hermes create connection stargaze gaia
 hermes create connection stargaze osmosis
-hermes create connection icad stargaze 
+hermes create connection stargaze icad
 hermes create channel --port-a transfer --port-b transfer stargaze connection-0
 hermes create channel --port-a transfer --port-b transfer stargaze connection-1
 hermes create channel --port-a transfer --port-b transfer stargaze connection-2
-hermes create channel --port-a icacontroller --port-b icahost icad connection-0
