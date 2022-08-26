6bcc628770c24b72d242cf4cf3e50bb11b723856
jhernandezb <contact@jhernandez.me>
2022-08-26 13:10:46 -0600

    fix chain id

--- scripts/ci/test-ica.sh
+++ scripts/ci/test-ica.sh
@@ -4,16 +4,16 @@ echo "$MNEMONIC" | icad keys add ica-test --recover --keyring-backend test
 ICA_WALLET_ADDRESS=$(icad keys show ica-test -a --keyring-backend test)
 echo $ICA_WALLET_ADDRESS
 
+export ICA_CHAIN_ID=icad
 icad config keyring-backend test
-icad config chain-id icad
+icad config chain-id $ICA_CHAIN_ID
 icad config node http://icad:26657
 icad status 
 icad query account $ICA_WALLET_ADDRESS --node http://icad:26657
 icad q bank balances $ICA_WALLET_ADDRESS --node http://icad:26657
-icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id ica --keyring-backend test  --generate-only >  tx.json  
+icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id $ICA_CHAIN_ID --keyring-backend test  --generate-only >  tx.json  
 cat tx.json | jq
-icad tx sign tx.json --from $ICA_WALLET_ADDRESS --chain-id ica --keyring-backend test 
-icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id ica --keyring-backend test --node http://icad:26657 -b block --yes
+icad tx intertx register --from $ICA_WALLET_ADDRESS --connection-id connection-0 --chain-id $ICA_CHAIN_ID --keyring-backend test --node http://icad:26657 -b block --yes
 icad query intertx interchainaccounts connection-0 $ICA_WALLET_ADDRESS --node http://icad:26657
 export ICA_ADDR=$(icad query intertx interchainaccounts connection-0 $ICA_WALLET_ADDRESS  -o json  --node http://icad:26657 | jq -r '.interchain_account_address') && echo $ICA_ADDR
 
