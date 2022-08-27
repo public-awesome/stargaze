af264b8cfc08627881a9fdfb5b0cf69092d603d6
jhernandezb <contact@jhernandez.me>
2022-08-26 22:34:39 -0600

    add denom traces migration

--- app/upgrades.go
+++ app/upgrades.go
@@ -15,10 +15,15 @@ import (
 	icacontrollertypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/controller/types"
 	icahosttypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/host/types"
 	icatypes "github.com/cosmos/ibc-go/v3/modules/apps/27-interchain-accounts/types"
+	ibctransfertypes "github.com/cosmos/ibc-go/v3/modules/apps/transfer/types"
 )
 
 // next upgrade name
 const upgradeName = "v7"
+
+func equalTraces(dtA, dtB ibctransfertypes.DenomTrace) bool {
+	return dtA.BaseDenom == dtB.BaseDenom && dtA.Path == dtB.Path
+}
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
@@ -51,6 +56,26 @@ func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 		}
 		// initialize ICS27 module
 		icamodule.InitModule(ctx, controllerParams, hostParams)
+
+		// fix denom traces
+		// list of traces that must replace the old traces in store
+		var newTraces []ibctransfertypes.DenomTrace
+		app.TransferKeeper.IterateDenomTraces(ctx,
+			func(dt ibctransfertypes.DenomTrace) bool {
+				// check if the new way of splitting FullDenom
+				// into Trace and BaseDenom passes validation and
+				// is the same as the current DenomTrace.
+				// If it isn't then store the new DenomTrace in the list of new traces.
+				newTrace := ibctransfertypes.ParseDenomTrace(dt.GetFullDenomPath())
+				if err := newTrace.Validate(); err == nil && !equalTraces(newTrace, dt) {
+					newTraces = append(newTraces, newTrace)
+				}
+				return false
+			})
+		// replace the outdated traces with the new trace information
+		for _, nt := range newTraces {
+			app.TransferKeeper.SetDenomTrace(ctx, nt)
+		}
 		return app.mm.RunMigrations(ctx, cfg, fromVM)
 	})
 
