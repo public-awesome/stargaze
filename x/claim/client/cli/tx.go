30515dfde33ff706a1d1698feaeb35e0588a7b94
jhernandezb <contact@jhernandez.me>
2022-09-10 11:33:42 -0600

    remove unused consts

--- x/claim/client/cli/tx.go
+++ x/claim/client/cli/tx.go
@@ -2,7 +2,6 @@ package cli
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/spf13/cobra"
 
@@ -10,8 +9,6 @@ import (
 	// "github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/public-awesome/stargaze/v7/x/claim/types"
 )
-
-var DefaultRelativePacketTimeoutTimestamp = uint64((time.Duration(10) * time.Minute).Nanoseconds())
 
 // GetTxCmd returns the transaction commands for this module
 func GetTxCmd() *cobra.Command {
