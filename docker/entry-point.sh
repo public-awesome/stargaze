8af8791732974bdc20025e9ba656d76770c2f815
Harry Hull <harry.hull1@gmail.com>
2022-09-15 11:30:58 -0700

    Make CHAINID and DENOM configurable
    (cherry picked from commit d18d2915c03d78a3a298e4195f1af9faf8ad96e8)

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -1,31 +1,24 @@
 #!/bin/sh
 
-CHAINID=$1
-GENACCT=$2
+CHAINID=${CHAINID:-testing}
+DENOM=${DENOM:-ustars}
 
-if [ -z "$1" ]; then
-  echo "Need to input chain id..."
-  exit 1
-fi
-
-if [ -z "$2" ]; then
-  echo "Need to input genesis account address..."
-  exit 1
-fi
+# TODO: Allow for multiple genesis account addrs to be passed in
 
 # Build genesis file incl account for passed address
-coins="10000000000000000stake"
+coins="10000000000000000$DENOM"
 starsd init --chain-id $CHAINID $CHAINID
 starsd keys add validator --keyring-backend="test"
 starsd add-genesis-account validator $coins --keyring-backend="test"
-starsd add-genesis-account $GENACCT $coins --keyring-backend="test"
-starsd gentx validator 10000000000stake --chain-id $CHAINID --keyring-backend="test"
+starsd add-genesis-account $1 $coins --keyring-backend="test"
+starsd gentx validator 10000000000$DENOM --chain-id $CHAINID --keyring-backend="test"
 starsd collect-gentxs
 
 
 # Set proper defaults and change ports
 sed -i 's/"leveldb"/"goleveldb"/g' ~/.starsd/config/config.toml
 sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.starsd/config/config.toml
+sed -i "s/\"stake\"/\"$DENOM\"/g" ~/.starsd/config/genesis.json
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.starsd/config/config.toml
 sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.starsd/config/config.toml
 sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.starsd/config/config.toml
