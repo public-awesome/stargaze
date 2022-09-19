9c0e4d21060e6e0afb1dcb3b218964ecca0578fe
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2022-09-19 11:20:15 +0530

    fixing alloc module typos

--- x/alloc/keeper/params.go
+++ x/alloc/keeper/params.go
@@ -5,13 +5,13 @@ import (
 	"github.com/public-awesome/stargaze/v7/x/alloc/types"
 )
 
-// GetParams returns the total set of minting parameters.
+// GetParams returns the total set of alloc module parameters.
 func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
 	k.paramstore.GetParamSet(ctx, &params)
 	return params
 }
 
-// SetParams sets the total set of minting parameters.
+// SetParams sets the total set of alloc module parameters.
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
 }
