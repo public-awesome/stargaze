350cb54398c76098066cb88af16f2ceb5ab6a3fd
Harry Hull <harry.hull1@gmail.com>
2022-12-12 13:16:22 -0800

    Set block.max_gas in Docker entry-point.sh

--- docker/entry-point.sh
+++ docker/entry-point.sh
@@ -2,6 +2,7 @@
 
 CHAINID=${CHAINID:-testing}
 DENOM=${DENOM:-ustars}
+BLOCK_GAS_LIMIT=${GAS_LIMIT:-75000000}
 
 # Build genesis file incl account for each address passed in
 coins="10000000000000000$DENOM"
@@ -23,6 +24,7 @@ starsd collect-gentxs
 sed -i 's/"leveldb"/"goleveldb"/g' ~/.starsd/config/config.toml
 sed -i 's#"tcp://127.0.0.1:26657"#"tcp://0.0.0.0:26657"#g' ~/.starsd/config/config.toml
 sed -i "s/\"stake\"/\"$DENOM\"/g" ~/.starsd/config/genesis.json
+sed -i "s/\"max_gas\": \"-1\"/\"max_gas\": \"$BLOCK_GAS_LIMIT\"/" ~/.starsd/config/genesis.json
 sed -i 's/timeout_commit = "5s"/timeout_commit = "1s"/g' ~/.starsd/config/config.toml
 sed -i 's/timeout_propose = "3s"/timeout_propose = "1s"/g' ~/.starsd/config/config.toml
 sed -i 's/index_all_keys = false/index_all_keys = true/g' ~/.starsd/config/config.toml
