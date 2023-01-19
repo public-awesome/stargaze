eb0635921d2ad249553d72636ff5ab360f2355a4
Spoorthi <spoorthi_s@ymail.com>
2023-01-19 17:57:10 +0000

    fixing linting errors

--- x/cron/keeper/keeper.go
+++ x/cron/keeper/keeper.go
@@ -3,33 +3,29 @@ package keeper
 import (
 	"fmt"
 
-	"github.com/tendermint/tendermint/libs/log"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	"github.com/cosmos/cosmos-sdk/codec"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/public-awesome/stargaze/v8/x/cron/types"
-	
+	"github.com/tendermint/tendermint/libs/log"
 )
 
 type (
 	Keeper struct {
-		
-		cdc      	codec.BinaryCodec
-		storeKey 	storetypes.StoreKey
-		memKey   	storetypes.StoreKey
-		paramstore	paramtypes.Subspace
-		
+		cdc        codec.BinaryCodec
+		storeKey   storetypes.StoreKey
+		memKey     storetypes.StoreKey
+		paramstore paramtypes.Subspace
 	}
 )
 
 func NewKeeper(
-    cdc codec.BinaryCodec,
-    storeKey,
-    memKey storetypes.StoreKey,
+	cdc codec.BinaryCodec,
+	storeKey,
+	memKey storetypes.StoreKey,
 	ps paramtypes.Subspace,
-    
-    
+
 ) *Keeper {
 	// set KeyTable if it has not already been set
 	if !ps.HasKeyTable() {
@@ -37,12 +33,11 @@ func NewKeeper(
 	}
 
 	return &Keeper{
-		
-		cdc:      	cdc,
-		storeKey: 	storeKey,
-		memKey:   	memKey,
-		paramstore:	ps,
-		
+
+		cdc:        cdc,
+		storeKey:   storeKey,
+		memKey:     memKey,
+		paramstore: ps,
 	}
 }
 
