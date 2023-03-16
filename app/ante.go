1061535c7c173b8d4c1e1ad2c489a62982b276d3
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2023-03-16 12:46:20 -0600

    bump ibc-4 and wasmd (#758)

--- app/ante.go
+++ app/ante.go
@@ -8,8 +8,8 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/x/auth/ante"
 	govkeeper "github.com/cosmos/cosmos-sdk/x/gov/keeper"
-	ibcante "github.com/cosmos/ibc-go/v3/modules/core/ante"
-	ibckeeper "github.com/cosmos/ibc-go/v3/modules/core/keeper"
+	ibcante "github.com/cosmos/ibc-go/v4/modules/core/ante"
+	ibckeeper "github.com/cosmos/ibc-go/v4/modules/core/keeper"
 	stargazeante "github.com/public-awesome/stargaze/v9/internal/ante"
 )
 
