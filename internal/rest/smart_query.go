daea1fb3c8eada92c894ff1914833c33bf97d8d2
jhernandezb <contact@jhernandez.me>
2023-03-20 18:10:37 -0600

    add responses

--- internal/rest/smart_query.go
+++ internal/rest/smart_query.go
@@ -11,22 +11,28 @@ import (
 // BatchedQuerierHandler returns a handler that performas batch queries to smart contracts.
 func BatchedQuerierHandler(clientCtx client.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
+			json.NewEncoder(w).Encode(QueryResponse{Error: "method not allowed"})
 			return
 		}
 		batchRequest := &BatchQueryRequest{}
 		err := json.NewDecoder(r.Body).Decode(batchRequest)
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(QueryResponse{Error: "invalid request"})
 			return
 		}
 		if len(batchRequest.QueryRequests) == 0 {
 			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(QueryResponse{Error: "empty request"})
 			return
 		}
 		if len(batchRequest.QueryRequests) > 10 {
+			json.NewEncoder(w).Encode(QueryResponse{Error: "max batch size"})
 			w.WriteHeader(http.StatusBadRequest)
+			return
 		}
 		querier := wasmtypes.NewQueryClient(clientCtx)
 		responses := make([]*BatchQueryResponse, len(batchRequest.QueryRequests))
@@ -43,13 +49,17 @@ func BatchedQuerierHandler(clientCtx client.Context) http.HandlerFunc {
 			}
 			responses[i] = batchQueryResponse
 		}
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(responses)
+		json.NewEncoder(w).Encode(QueryResponse{Results: responses})
 	}
 }
 
 type BatchQueryRequest struct {
-	QueryRequests []wasmtypes.QuerySmartContractStateRequest `json:"query_requests"`
+	QueryRequests []wasmtypes.QuerySmartContractStateRequest `json:"requests"`
+}
+
+type QueryResponse struct {
+	Error   string                `json:"error,omitempty"`
+	Results []*BatchQueryResponse `json:"results,omitempty"`
 }
 
 type BatchQueryResponse struct {
