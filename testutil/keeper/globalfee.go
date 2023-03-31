82f73807186c2a4250c9550e1507aff44645620c
Spoorthi <spoorthi_s@ymail.com>
2023-03-31 12:58:36 +0000

    adding msg srvr tests

--- testutil/keeper/globalfee.go
+++ testutil/keeper/globalfee.go
@@ -8,10 +8,12 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
+	paramskeeper "github.com/cosmos/cosmos-sdk/x/params/keeper"
+	"github.com/public-awesome/stargaze/v9/app"
 	"github.com/public-awesome/stargaze/v9/x/globalfee/keeper"
 	"github.com/public-awesome/stargaze/v9/x/globalfee/types"
 	"github.com/stretchr/testify/require"
+	"github.com/tendermint/spm/cosmoscmd"
 	"github.com/tendermint/tendermint/libs/log"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmdb "github.com/tendermint/tm-db"
@@ -21,29 +23,33 @@ import (
 func GlobalFeeKeeper(t testing.TB) (keeper.Keeper, sdk.Context) {
 	storeKey := sdk.NewKVStoreKey(types.StoreKey)
 	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
+	tStoreKey := storetypes.NewTransientStoreKey("t_globalfee")
 
 	db := tmdb.NewMemDB()
 	stateStore := store.NewCommitMultiStore(db)
 	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
 	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
+	stateStore.MountStoreWithDB(tStoreKey, sdk.StoreTypeTransient, db)
 	require.NoError(t, stateStore.LoadLatestVersion())
 
 	registry := codectypes.NewInterfaceRegistry()
-	cdc := codec.NewProtoCodec(registry)
+	encoding := cosmoscmd.MakeEncodingConfig(app.ModuleBasics)
+	appCodec := encoding.Marshaler
 
-	paramsSubspace := typesparams.NewSubspace(cdc,
-		types.Amino,
-		storeKey,
-		memStoreKey,
-		"GlobalFeeParams",
-	)
+	paramsKeeper := paramskeeper.NewKeeper(appCodec, encoding.Amino, storeKey, tStoreKey)
+	paramsKeeper.Subspace(types.ModuleName).WithKeyTable(types.ParamKeyTable())
+	subspace, _ := paramsKeeper.GetSubspace(types.ModuleName)
+
 	k := keeper.NewKeeper(
-		cdc,
+		codec.NewProtoCodec(registry),
 		storeKey,
-		paramsSubspace,
+		subspace,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+
+	params := types.Params{PrivilegedAddress: []string{}}
+	k.SetParams(ctx, params)
 
 	return k, ctx
 }
