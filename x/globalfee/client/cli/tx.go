44704c4890ddc664afeecb9de805d75526a623ed
Spoorthi <spoorthi_s@ymail.com>
2023-04-05 12:46:23 +0000

    adding code authorization tx

--- x/globalfee/client/cli/tx.go
+++ x/globalfee/client/cli/tx.go
@@ -1,10 +1,151 @@
 package cli
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
+	"github.com/public-awesome/stargaze/v9/x/globalfee/types"
 	"github.com/spf13/cobra"
 )
 
 // GetTxCmd builds tx command group for the module.
 func GetTxCmd() *cobra.Command {
-	panic("unimplemented 👻")
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	cmd.AddCommand(CmdSetCodeAuthorization())
+	cmd.AddCommand(CmdRemoveCodeAuthorization())
+	cmd.AddCommand(CmdInitialClaim())
+	cmd.AddCommand(CmdInitialClaim())
+
+	return cmd
+}
+
+func CmdSetCodeAuthorization() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "set-code-authorization [code-id] [methods]",
+		Short: "",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			codeId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			methods := strings.Split(args[1], ",")
+
+			msg := types.NewMsgSetCodeAuthorization(
+				clientCtx.GetFromAddress().String(),
+				codeId,
+				methods,
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdRemoveCodeAuthorization() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "remove-code-authorization [code-id]",
+		Short: "",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			codeId, err := strconv.ParseUint(args[0], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			msg := types.MsgRemoveCodeAuthorization{
+				Sender: clientCtx.GetFromAddress().String(),
+				CodeId: codeId,
+			}
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdInitialClaim() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "initial-claim",
+		Short: "Claim Initial Amount",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgInitialClaim(
+				clientCtx.GetFromAddress().String(),
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+func CmdInitialClaim() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "initial-claim",
+		Short: "Claim Initial Amount",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewMsgInitialClaim(
+				clientCtx.GetFromAddress().String(),
+			)
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
 }
