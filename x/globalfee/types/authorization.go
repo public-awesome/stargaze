aed0ac704eba227dff2d5ab06a19ab29ffb18cad
Spoorthi <spoorthi_s@ymail.com>
2023-04-06 16:19:02 +0000

    fixing linting

--- x/globalfee/types/authorization.go
+++ x/globalfee/types/authorization.go
@@ -8,21 +8,24 @@ import (
 )
 
 func validateMethods(caMethods []string) error {
-	if len(caMethods) <= 0 {
+	if len(caMethods) == 0 {
 		return sdkErrors.Wrap(ErrInvalidMethods, "empty methods provided")
 	}
 
+	pattern := `^(\*|[\w]+)$` // only allow method names or "*"
+	matcher, err := regexp.Compile(pattern)
+	if err != nil {
+		return err
+	}
 	for _, method := range caMethods {
 		if len(strings.TrimSpace(method)) == 0 {
 			return sdkErrors.Wrap(ErrInvalidMethods, "method name is empty")
 		}
 
-		pattern := `^(\*|[\w]+)$`
-		matchFound, err := regexp.MatchString(pattern, method)
 		if err != nil {
 			return sdkErrors.Wrap(err, "")
 		}
-		if !matchFound {
+		if !matcher.MatchString(method) {
 			return sdkErrors.Wrap(ErrInvalidMethods, "invalid method name")
 		}
 	}
