f4d8263f3f7e98ef0ee0a44ee6b4e30231e7e077
Spoorthi <spoorthi_s@ymail.com>
2023-04-06 09:12:59 +0000

    adding tests for checking valid codeid and contrataddr

--- testutil/keeper/globalfee.go
+++ testutil/keeper/globalfee.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"testing"
 
+	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	"github.com/cosmos/cosmos-sdk/store"
@@ -37,10 +38,33 @@ func GlobalFeeKeeper(t testing.TB) (keeper.Keeper, sdk.Context) {
 		memStoreKey,
 		"GlobalFeeParams",
 	)
+	wk := MockWasmKeeper{
+		HasContractInfoFn: func(ctx sdk.Context, contractAddr sdk.AccAddress) bool {
+			switch contractAddr.String() {
+			case "cosmos1qyqszqgpqyqszqgpqyqszqgpqyqszqgpjnp7du":
+				return true
+			case "cosmos1hfml4tzwlc3mvynsg6vtgywyx00wfkhrtpkx6t":
+				return true
+			case "cosmos144sh8vyv5nqfylmg4mlydnpe3l4w780jsrmf4k":
+				return true
+			}
+			return false
+		},
+		GetCodeInfoFn: func(ctx sdk.Context, codeID uint64) *wasmtypes.CodeInfo {
+			if codeID == 1 {
+				return &wasmtypes.CodeInfo{
+					Creator: "cosmos144sh8vyv5nqfylmg4mlydnpe3l4w780jsrmf4k",
+				}
+			}
+			return nil
+		},
+	}
+
 	k := keeper.NewKeeper(
 		cdc,
 		storeKey,
 		paramsSubspace,
+		wk,
 	)
 
 	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
