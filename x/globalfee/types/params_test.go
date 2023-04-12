c17132e98942bd186c67661a75e64768fd2a8824
Spoorthi <spoorthi_s@ymail.com>
2023-04-12 09:21:37 +0000

    adding mingas fees types

--- x/globalfee/types/params_test.go
+++ x/globalfee/types/params_test.go
@@ -3,6 +3,7 @@ package types_test
 import (
 	"testing"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/public-awesome/stargaze/v9/x/globalfee/types"
 	"github.com/stretchr/testify/require"
 )
@@ -26,6 +27,34 @@ func TestParamsValidate(t *testing.T) {
 		{
 			"ok: valid addr",
 			types.Params{PrivilegedAddress: []string{"cosmos1c4k24jzduc365kywrsvf5ujz4ya6mwymy8vq4q"}},
+			false,
+		},
+		{
+			"fail: zero fees",
+			types.Params{
+				MinimumGasPrices: sdk.NewDecCoinsFromCoins(sdk.NewCoin("stars", sdk.ZeroInt())),
+			},
+			true,
+		},
+		{
+			"fail: duplicate denom fees",
+			types.Params{
+				MinimumGasPrices: sdk.DecCoins{sdk.NewDecCoin("stars", sdk.OneInt()), sdk.NewDecCoin("stars", sdk.OneInt())},
+			},
+			true,
+		},
+		{
+			"fail: unordered by denom",
+			types.Params{
+				MinimumGasPrices: sdk.DecCoins{sdk.NewDecCoin("stars", sdk.OneInt()), sdk.NewDecCoin("atom", sdk.OneInt())},
+			},
+			true,
+		},
+		{
+			"ok: valid min gas fees",
+			types.Params{
+				MinimumGasPrices: sdk.DecCoins{sdk.NewDecCoin("atom", sdk.OneInt()), sdk.NewDecCoin("stars", sdk.OneInt())},
+			},
 			false,
 		},
 	}
