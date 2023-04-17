21bf47572624322dc362cef965cbea755508c8eb
Spoorthi <spoorthi_s@ymail.com>
2023-04-17 10:27:17 +0000

    adding prop interfaces for amino & registry

--- x/globalfee/types/codec.go
+++ x/globalfee/types/codec.go
@@ -5,15 +5,37 @@ import (
 	"github.com/cosmos/cosmos-sdk/codec/types"
 	cryptoCodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
 // RegisterLegacyAminoCodec registers the necessary interfaces and concrete types on the provided LegacyAmino codec.
 // These types are used for Amino JSON serialization.
 func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgSetCodeAuthorization{}, "globalfee/MsgSetCodeAuthorization", nil)
+	cdc.RegisterConcrete(&MsgRemoveCodeAuthorization{}, "globalfee/MsgRemoveCodeAuthorization", nil)
+	cdc.RegisterConcrete(&MsgSetContractAuthorization{}, "globalfee/MsgSetContractAuthorization", nil)
+	cdc.RegisterConcrete(&MsgRemoveContractAuthorization{}, "globalfee/MsgRemoveContractAuthorization", nil)
+
+	cdc.RegisterConcrete(&SetCodeAuthorizationProposal{}, "globalfee/SetCodeAuthorizationProposal", nil)
+	cdc.RegisterConcrete(&RemoveCodeAuthorizationProposal{}, "globalfee/RemoveCodeAuthorizationProposal", nil)
+	cdc.RegisterConcrete(&SetContractAuthorizationProposal{}, "globalfee/SetContractAuthorizationProposal", nil)
+	cdc.RegisterConcrete(&RemoveContractAuthorizationProposal{}, "globalfee/RemoveContractAuthorizationProposal", nil)
 }
 
 // RegisterInterfaces registers interfaces types with the interface registry.
 func RegisterInterfaces(registry types.InterfaceRegistry) {
+	registry.RegisterImplementations((*sdk.Msg)(nil),
+		&MsgSetCodeAuthorization{},
+		&MsgRemoveCodeAuthorization{},
+		&MsgSetContractAuthorization{},
+		&MsgRemoveContractAuthorization{},
+
+		&SetCodeAuthorizationProposal{},
+		&RemoveCodeAuthorizationProposal{},
+		&SetContractAuthorizationProposal{},
+		&RemoveContractAuthorizationProposal{},
+	)
+	msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
 }
 
 var (
