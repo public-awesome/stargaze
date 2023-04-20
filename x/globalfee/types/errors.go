24ee77872395c15ef4a415a915d23fe9b804be7b
Spoorthi <spoorthi_s@ymail.com>
2023-04-20 12:11:41 +0000

    fixing pr review comments

--- x/globalfee/types/errors.go
+++ x/globalfee/types/errors.go
@@ -4,7 +4,7 @@ import sdkErrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
 	DefaultCodespace    = ModuleName
-	ErrInvalidMethods   = sdkErrors.Register(DefaultCodespace, 0, "invalid method in code/contract authorization") // Code or Contract Authorizations have invalid methods configured
-	ErrContractNotExist = sdkErrors.Register(DefaultCodespace, 1, "contract with given address does not exist")
-	ErrCodeIdNotExist   = sdkErrors.Register(DefaultCodespace, 2, "code id does not exist")
+	ErrInvalidMethods   = sdkErrors.Register(DefaultCodespace, 2, "invalid method in code/contract authorization") // Code or Contract Authorizations have invalid methods configured
+	ErrContractNotExist = sdkErrors.Register(DefaultCodespace, 3, "contract with given address does not exist")
+	ErrCodeIdNotExist   = sdkErrors.Register(DefaultCodespace, 4, "code id does not exist")
 )
