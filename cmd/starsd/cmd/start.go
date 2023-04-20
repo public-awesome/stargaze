460c9669867129f28d1cdb2da1ff7463f98a5660
jhernandezb <contact@jhernandez.me>
2023-04-19 21:00:51 -0600

    fix libwasm version

--- cmd/starsd/cmd/start.go
+++ cmd/starsd/cmd/start.go
@@ -8,7 +8,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var LibwasmVersion = "1.2.2"
+var LibwasmVersion = "1.2.3"
 
 func CheckLibwasmVersion(cmd *cobra.Command, args []string) error {
 	version, err := wasmvmapi.LibwasmvmVersion()
