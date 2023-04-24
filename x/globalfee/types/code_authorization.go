a9c02a10c885c66c07a584923b13e5e77f9d482d
jhernandezb <contact@jhernandez.me>
2023-04-23 18:22:32 -0600

    fix duplicate

--- x/globalfee/types/code_authorization.go
+++ x/globalfee/types/code_authorization.go
@@ -5,11 +5,6 @@ import (
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
-// Validate validates the values of code authorizations
-func (ca CodeAuthorization) Validate() error {
-	return validateMethods(ca.Methods)
-}
-
 var _ sdk.Msg = &MsgSetCodeAuthorization{}
 var _ sdk.Msg = &MsgRemoveCodeAuthorization{}
 
@@ -94,6 +89,7 @@ func (msg MsgRemoveCodeAuthorization) ValidateBasic() error {
 	return nil
 }
 
+// Validate validates the values of code authorizations
 func (ca CodeAuthorization) Validate() error {
 	return validateMethods(ca.Methods)
 }
