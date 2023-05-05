0949043eb5caea2eda72dffc45750c026b8ec214
Spoorthi <spoorthi_s@ymail.com>
2023-05-05 09:54:19 +0000

    adding tokenfactory types

---
+++ x/tokenfactory/types/genesis_test.go
@@ -0,0 +1,138 @@
+package types_test
+
+import (
+	"testing"
+
+	"github.com/public-awesome/stargaze/v10/x/tokenfactory/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesisState_Validate(t *testing.T) {
+	for _, tc := range []struct {
+		desc     string
+		genState *types.GenesisState
+		valid    bool
+	}{
+		{
+			desc:     "default is valid",
+			genState: types.DefaultGenesis(),
+			valid:    true,
+		},
+		{
+			desc: "valid genesis state",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1t7egva48prqmzl59x5ngv4zx0dtrwewcdqdjr8/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "cosmos1t7egva48prqmzl59x5ngv4zx0dtrwewcdqdjr8",
+						},
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "different admin from creator",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1t7egva48prqmzl59x5ngv4zx0dtrwewcdqdjr8/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f",
+						},
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "empty admin",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "",
+						},
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "no admin",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "invalid admin",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "👻",
+						},
+					},
+				},
+			},
+			valid: false,
+		},
+		{
+			desc: "multiple denoms",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "",
+						},
+					},
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/beepboop",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "",
+						},
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "duplicate denoms",
+			genState: &types.GenesisState{
+				FactoryDenoms: []types.GenesisDenom{
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "",
+						},
+					},
+					{
+						Denom: "factory/cosmos1rf3renzcj8m2pav74758lj7wm8z98yky20x64f/testy",
+						AuthorityMetadata: types.DenomAuthorityMetadata{
+							Admin: "",
+						},
+					},
+				},
+			},
+			valid: false,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := tc.genState.Validate()
+			if tc.valid {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+			}
+		})
+	}
+}
