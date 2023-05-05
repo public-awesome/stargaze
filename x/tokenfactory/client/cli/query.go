b60da3883be18786db245382327f36ddd57ff546
Spoorthi <spoorthi_s@ymail.com>
2023-05-05 10:21:06 +0000

    adding module.go

--- x/tokenfactory/client/cli/query.go
+++ x/tokenfactory/client/cli/query.go
@@ -11,7 +11,7 @@ import (
 )
 
 // GetQueryCmd returns the cli query commands for this module
-func GetQueryCmd(_ string) *cobra.Command {
+func GetQueryCmd() *cobra.Command {
 	// Group tokenfactory queries under a subcommand
 	cmd := &cobra.Command{
 		Use:                        types.ModuleName,
