036001054d0063c7b21d2f25928f9b56935fd16b
jhernandezb <contact@jhernandez.me>
2023-05-30 21:26:16 -0600

    emit events to parent context

--- x/cron/abci.go
+++ x/cron/abci.go
@@ -52,6 +52,7 @@ func abciContractCallback(parentCtx sdk.Context, w types.WasmKeeper, msg contrac
 			)
 			return false // return without commit
 		}
+		parentCtx.EventManager().EmitEvents(ctx.EventManager().Events())
 		commit()
 		return false
 	}
