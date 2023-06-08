81a62f9eb9089fbf776985251c84063cf6d58834
Spoorthi <spoorthi_s@ymail.com>
2023-06-08 12:02:09 +0000

    reset upgrade handler

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,13 +7,10 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-
-	crontypes "github.com/public-awesome/stargaze/v11/x/cron/types"
-	globalfeetypes "github.com/public-awesome/stargaze/v11/x/globalfee/types"
 )
 
 // next upgrade name
-const upgradeName = "v10"
+const upgradeName = "v11"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
@@ -23,17 +20,6 @@ func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 		if err != nil {
 			return nil, err
 		}
-		// set global fee params to 1ustars
-		stakeDenom := app.StakingKeeper.BondDenom(ctx)
-		globalfeeParams := app.GlobalFeeKeeper.GetParams(ctx)
-		globalfeeParams.MinimumGasPrices = sdk.NewDecCoins(sdk.NewDecCoinFromDec(stakeDenom, sdk.OneDec()))
-
-		globalfeeParams.PrivilegedAddresses = []string{
-			// Multisig that is in charge of managing Launchpad Factories until a DAO is created
-			// https://www.mintscan.io/stargaze/account/stars1zmaulflshft579x37acrdad72r57vnn5zsn0ee
-			"stars1zmaulflshft579x37acrdad72r57vnn5zsn0ee",
-		}
-		app.GlobalFeeKeeper.SetParams(ctx, globalfeeParams)
 		return migrations, nil
 	})
 
@@ -43,9 +29,7 @@ func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
 	}
 
 	if upgradeInfo.Name == upgradeName && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := store.StoreUpgrades{
-			Added: []string{crontypes.ModuleName, globalfeetypes.ModuleName},
-		}
+		storeUpgrades := store.StoreUpgrades{}
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
