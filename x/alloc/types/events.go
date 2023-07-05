f03a7f57d08b537d8d10cc658bb5268b2a53af42
jhernandezb <contact@jhernandez.me>
2023-07-05 15:39:51 -0600

    emit distribution event

--- x/alloc/types/events.go
+++ x/alloc/types/events.go
@@ -1,6 +1,12 @@
 package types
 
 const (
-	EventTypeFundFairburnPool = "fund_fairburn_pool"
-	AttributeValueCategory    = ModuleName
+	EventTypeFundFairburnPool       = "fund_fairburn_pool"
+	EventTypeDistribution           = "alloc_distribution"
+	AttributeValueCategory          = ModuleName
+	AttributeKeySupplementAmount    = "supplement_amount"
+	AttributeKeyCommunityPoolAmount = "community_pool_amount"
+	AttributeKeyIncentivesAmount    = "incentives_amount"
+	AttributeKeyDevRewardsAmount    = "dev_rewards_amount"
+	AttributeKeyFeePoolAmount       = "fee_pool_amount"
 )
