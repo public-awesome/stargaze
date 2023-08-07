8e07f02e514d303fdaefc145edc73fe9e46b1317
Spoorthi <spoorthi_s@ymail.com>
2023-08-07 10:40:25 +0000

    updating wasmd, cosmos sdk, ibc-go

--- x/ibchooks/wasm_hook.go
+++ x/ibchooks/wasm_hook.go
@@ -13,9 +13,9 @@ import (
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	transfertypes "github.com/cosmos/ibc-go/v4/modules/apps/transfer/types"
-	channeltypes "github.com/cosmos/ibc-go/v4/modules/core/04-channel/types"
-	ibcexported "github.com/cosmos/ibc-go/v4/modules/core/exported"
+	transfertypes "github.com/cosmos/ibc-go/v7/modules/apps/transfer/types"
+	channeltypes "github.com/cosmos/ibc-go/v7/modules/core/04-channel/types"
+	ibcexported "github.com/cosmos/ibc-go/v7/modules/core/exported"
 )
 
 type ContractAck struct {
