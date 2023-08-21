5ea0417c52eaf139ddc48b9e3f0d4423c81766aa
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2023-08-21 12:51:53 -0600

    bump wasmd (#857)
    * bump wasmd
    
    * Update app/wasm.go
    
    Co-authored-by: Spoorthi <9302666+spoo-bar@users.noreply.github.com>
    
    * fix keeper initialization
    
    * update accepted queries list
    
    * fix linter
    
    * display images
    
    * update libwasmvm in docker
    
    * update drone sig
    
    * bump PFM
    
    ---------
    
    Co-authored-by: Spoorthi <9302666+spoo-bar@users.noreply.github.com>

--- app/app.go
+++ app/app.go
@@ -126,6 +126,7 @@ import (
 
 	packetforward "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v4/router"
 	packetforwardkeeper "github.com/cosmos/ibc-apps/middleware/packet-forward-middleware/v4/router/keeper"
+
 	//  ica
 	ica "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts"
 	icahost "github.com/cosmos/ibc-go/v4/modules/apps/27-interchain-accounts/host"
@@ -579,18 +580,11 @@ func NewStargazeApp(
 	registry.RegisterEncoder(sgwasm.DistributionRoute, sgwasm.CustomDistributionEncoder)
 	registry.RegisterEncoder(allocmoduletypes.ModuleName, allocwasm.Encoder)
 
-	// Wasm accepted Stargate Queries
-	acceptStargateQueriesList := wasmkeeper.AcceptedStargateQueries{
-		"/stargaze.tokenfactory.v1.Query/Params":                 &tokenfactorytypes.QueryParamsResponse{},
-		"/stargaze.tokenfactory.v1.Query/DenomAuthorityMetadata": &tokenfactorytypes.QueryDenomAuthorityMetadataResponse{},
-		"/stargaze.tokenfactory.v1.Query/DenomsFromCreator":      &tokenfactorytypes.QueryDenomsFromCreatorResponse{},
-	}
-
 	wasmOpts = append(
 		wasmOpts,
 		wasmkeeper.WithMessageEncoders(sgwasm.MessageEncoders(registry)),
 		wasmkeeper.WithQueryPlugins(&wasmkeeper.QueryPlugins{
-			Stargate: wasmkeeper.AcceptListStargateQuerier(acceptStargateQueriesList, app.GRPCQueryRouter(), appCodec),
+			Stargate: wasmkeeper.AcceptListStargateQuerier(AcceptedStargateQueries(), app.GRPCQueryRouter(), appCodec),
 		}),
 	)
 	app.WasmKeeper = wasm.NewKeeper(
@@ -601,6 +595,7 @@ func NewStargazeApp(
 		app.BankKeeper,
 		app.StakingKeeper,
 		app.DistrKeeper,
+		app.IBCKeeper.ChannelKeeper,
 		app.IBCKeeper.ChannelKeeper,
 		&app.IBCKeeper.PortKeeper,
 		scopedWasmKeeper,
