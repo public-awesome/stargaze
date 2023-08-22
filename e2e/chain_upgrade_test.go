83755ba6eb68dc5193f938aa75cfbdcd263ebfdf
Spoorthi <spoorthi_s@ymail.com>
2023-08-22 04:54:49 +0000

    refactoring the tests

--- e2e/chain_upgrade_test.go
+++ e2e/chain_upgrade_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/docker/docker/client"
 	interchaintest "github.com/strangelove-ventures/interchaintest/v4"
 	"github.com/strangelove-ventures/interchaintest/v4/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v4/ibc"
@@ -14,15 +15,16 @@ import (
 )
 
 const (
-	haltHeightDelta    = uint64(10) // will propose upgrade this many blocks in the future
-	blocksAfterUpgrade = uint64(10)
-	votingPeriod       = "10s"
-	maxDepositPeriod   = "10s"
+	initialVersion = "v11.0.0" // The last released git tag version of the stargaze binary. This version of the image is fetched from Heigliner backage repository.
+	upgradeName    = "v12"     // The upcoming version name - Should match with upgrade handler name. This version needs to be built locally for tests. Using `make build-docker`
 )
 
 const (
-	initialVersion = "v11.0.0"
-	upgradeVersion = "v12"
+	haltHeightDelta    = uint64(10) // The number of blocks after which to apply upgrade after creation of proposal.
+	blocksAfterUpgrade = uint64(10) // The number of blocks to wait for after the upgrade has been applied.
+	votingPeriod       = "10s"      // Reducing voting period for testing
+	maxDepositPeriod   = "10s"      // Reducing max deposit period for testing
+	depositDenom       = "ustars"   // The bond denom to be used to deposit for propsals
 )
 
 func TestChainUpgrade(t *testing.T) {
@@ -30,6 +32,78 @@ func TestChainUpgrade(t *testing.T) {
 		t.Skip("skipping in short mode")
 	}
 
+	stargazeChain, client, ctx := startChain(t)
+	chainUser := fundChainUser(t, ctx, stargazeChain)
+	haltHeight := submitUpgradeProposalAndVote(t, ctx, stargazeChain, chainUser)
+
+	height, err := stargazeChain.Height(ctx)
+	require.NoError(t, err, "error fetching height before upgrade")
+
+	timeoutCtx, timeoutCtxCancel := context.WithTimeout(ctx, time.Second*45)
+	defer timeoutCtxCancel()
+
+	// This should timeout due to chain halt at upgrade height.
+	_ = testutil.WaitForBlocks(timeoutCtx, int(haltHeight-height)+1, stargazeChain)
+
+	height, err = stargazeChain.Height(ctx)
+	require.NoError(t, err, "error fetching height after chain should have halted")
+
+	// Make sure that chain is halted
+	require.Equal(t, haltHeight, height, "height is not equal to halt height")
+
+	// Bring down nodes to prepare for upgrade
+	err = stargazeChain.StopAllNodes(ctx)
+	require.NoError(t, err, "error stopping node(s)")
+
+	// Upgrade version on all nodes - We are passing in the local image for the upgrade build using `make build-docker`
+	stargazeChain.UpgradeVersion(ctx, client, "publicawesome/stargaze", "local-dev")
+
+	// Start all nodes back up.
+	// Validators reach consensus on first block after upgrade height
+	// And chain block production resumes 🎉
+	err = stargazeChain.StartAllNodes(ctx)
+	require.NoError(t, err, "error starting upgraded node(s)")
+
+	timeoutCtx, timeoutCtxCancel = context.WithTimeout(ctx, time.Second*45)
+	defer timeoutCtxCancel()
+
+	err = testutil.WaitForBlocks(timeoutCtx, int(blocksAfterUpgrade), stargazeChain)
+	require.NoError(t, err, "chain did not produce blocks after upgrade")
+}
+
+func submitUpgradeProposalAndVote(t *testing.T, ctx context.Context, stargazeChain *cosmos.CosmosChain, chainUser ibc.Wallet) uint64 {
+	height, err := stargazeChain.Height(ctx) // The current chain height
+	require.NoError(t, err, "error fetching height before submit upgrade proposal")
+
+	haltHeight := height + haltHeightDelta // The height at which upgrade should be applied
+
+	proposal := cosmos.SoftwareUpgradeProposal{
+		Deposit:     "10000000000" + stargazeChain.Config().Denom,
+		Title:       "Chain Upgrade 1",
+		Name:        upgradeName,
+		Description: "First chain software upgrade",
+		Height:      haltHeight,
+	}
+
+	upgradeTx, err := stargazeChain.UpgradeProposal(ctx, chainUser.KeyName(), proposal) // Submitting the software upgrade proposal
+	require.NoError(t, err, "error submitting software upgrade proposal tx")
+
+	err = stargazeChain.VoteOnProposalAllValidators(ctx, upgradeTx.ProposalID, cosmos.ProposalVoteYes)
+	require.NoError(t, err, "failed to submit votes")
+
+	_, err = cosmos.PollForProposalStatus(ctx, stargazeChain, height, height+haltHeightDelta, upgradeTx.ProposalID, cosmos.ProposalStatusPassed)
+	require.NoError(t, err, "proposal status did not change to passed in expected number of blocks")
+	return haltHeight
+}
+
+func fundChainUser(t *testing.T, ctx context.Context, stargazeChain *cosmos.CosmosChain) ibc.Wallet {
+	const userFunds = int64(10_000_000_000_000)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, stargazeChain)
+	return users[0]
+}
+
+func startChain(t *testing.T) (*cosmos.CosmosChain, *client.Client, context.Context) {
+	// Configuring the chain factory. We are building Stargaze chain with the version that matches the `initialVersion` value
 	numOfVals := 5
 	cf := interchaintest.NewBuiltinChainFactory(zaptest.NewLogger(t), []*interchaintest.ChainSpec{
 		{
@@ -37,18 +111,16 @@ func TestChainUpgrade(t *testing.T) {
 			ChainName: "stargaze-1",
 			Version:   initialVersion,
 			ChainConfig: ibc.ChainConfig{
-				ModifyGenesis: cosmos.ModifyGenesis(getTestGenesis()),
+				ModifyGenesis: cosmos.ModifyGenesis(getTestGenesis()), // Modifying genesis to have test-friendly gov params
 			},
 			NumValidators: &numOfVals,
 		},
 	})
-
 	chains, err := cf.Chains(t.Name())
 	require.NoError(t, err)
 	stargazeChain := chains[0].(*cosmos.CosmosChain)
 
 	ic := interchaintest.NewInterchain().AddChain(stargazeChain)
-
 	client, network := interchaintest.DockerSetup(t)
 	ctx := context.Background()
 	require.NoError(t, ic.Build(ctx, nil, interchaintest.InterchainBuildOptions{
@@ -60,66 +132,7 @@ func TestChainUpgrade(t *testing.T) {
 	t.Cleanup(func() {
 		_ = ic.Close()
 	})
-
-	const userFunds = int64(10_000_000_000_000)
-	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, stargazeChain)
-	chainUser := users[0]
-
-	height, err := stargazeChain.Height(ctx)
-	require.NoError(t, err, "error fetching height before submit upgrade proposal")
-
-	haltHeight := height + haltHeightDelta
-
-	proposal := cosmos.SoftwareUpgradeProposal{
-		Deposit:     "10000000000" + stargazeChain.Config().Denom, // greater than min deposit
-		Title:       "Chain Upgrade 1",
-		Name:        upgradeVersion,
-		Description: "First chain software upgrade",
-		Height:      haltHeight,
-	}
-
-	upgradeTx, err := stargazeChain.UpgradeProposal(ctx, chainUser.KeyName(), proposal)
-	require.NoError(t, err, "error submitting software upgrade proposal tx")
-
-	err = stargazeChain.VoteOnProposalAllValidators(ctx, upgradeTx.ProposalID, cosmos.ProposalVoteYes)
-	require.NoError(t, err, "failed to submit votes")
-
-	_, err = cosmos.PollForProposalStatus(ctx, stargazeChain, height, height+haltHeightDelta, upgradeTx.ProposalID, cosmos.ProposalStatusPassed)
-	require.NoError(t, err, "proposal status did not change to passed in expected number of blocks")
-
-	height, err = stargazeChain.Height(ctx)
-	require.NoError(t, err, "error fetching height before upgrade")
-
-	timeoutCtx, timeoutCtxCancel := context.WithTimeout(ctx, time.Second*45)
-	defer timeoutCtxCancel()
-
-	// this should timeout due to chain halt at upgrade height.
-	_ = testutil.WaitForBlocks(timeoutCtx, int(haltHeight-height)+1, stargazeChain)
-
-	height, err = stargazeChain.Height(ctx)
-	require.NoError(t, err, "error fetching height after chain should have halted")
-
-	// make sure that chain is halted
-	require.Equal(t, haltHeight, height, "height is not equal to halt height")
-
-	// bring down nodes to prepare for upgrade
-	err = stargazeChain.StopAllNodes(ctx)
-	require.NoError(t, err, "error stopping node(s)")
-
-	// upgrade version on all nodes
-	stargazeChain.UpgradeVersion(ctx, client, "publicawesome/stargaze", "local-dev")
-
-	// start all nodes back up.
-	// validators reach consensus on first block after upgrade height
-	// and chain block production resumes.
-	err = stargazeChain.StartAllNodes(ctx)
-	require.NoError(t, err, "error starting upgraded node(s)")
-
-	timeoutCtx, timeoutCtxCancel = context.WithTimeout(ctx, time.Second*45)
-	defer timeoutCtxCancel()
-
-	err = testutil.WaitForBlocks(timeoutCtx, int(blocksAfterUpgrade), stargazeChain)
-	require.NoError(t, err, "chain did not produce blocks after upgrade")
+	return stargazeChain, client, ctx
 }
 
 func getTestGenesis() []cosmos.GenesisKV {
@@ -134,7 +147,7 @@ func getTestGenesis() []cosmos.GenesisKV {
 		},
 		{
 			Key:   "app_state.gov.deposit_params.min_deposit.0.denom",
-			Value: "ustars",
+			Value: depositDenom,
 		},
 	}
 }
