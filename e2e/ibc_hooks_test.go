df951b794c081a0e55ddc121bf1ba8b4ccdbc92f
Spoorthi <spoorthi_s@ymail.com>
2023-09-08 06:06:00 +0000

    init the contract

--- e2e/ibc_hooks_test.go
+++ e2e/ibc_hooks_test.go
@@ -2,8 +2,10 @@ package e2e
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	interchaintest "github.com/strangelove-ventures/interchaintest/v4"
 	"github.com/strangelove-ventures/interchaintest/v4/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v4/ibc"
@@ -11,6 +13,7 @@ import (
 	"github.com/strangelove-ventures/interchaintest/v4/testutil"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap/zaptest"
+	"gopkg.in/yaml.v2"
 )
 
 func TestIBCHooks(t *testing.T) {
@@ -93,44 +96,75 @@ func TestIBCHooks(t *testing.T) {
 
 	const userFunds = int64(10_000_000_000)
 	users := interchaintest.GetAndFundTestUsers(t, ctx, t.Name(), userFunds, chainA, chainB)
-	//user1 := users[0]
+	user1 := users[0]
 	user2 := users[1]
 
 	// Wait a few blocks for relayer to start and for user accounts to be created
 	err = testutil.WaitForBlocks(ctx, 5, chainA, chainB)
 	require.NoError(t, err)
 
-	// Store the test contract on Chain B
+	// Store and init the test contract on Chain B
+	contractAddress := setupCounterContract(ctx, chainB, user2, t)
+
+	channel, err := ibc.GetTransferChannel(ctx, r, eRep, chainA.Config().ChainID, chainB.Config().ChainID)
+	require.NoError(t, err)
+
+	transfer := ibc.WalletAmount{
+		Address: contractAddress,
+		Denom:   chainA.Config().Denom,
+		Amount:  int64(1),
+	}
+
+	memo := ibc.TransferOptions{
+		Memo: fmt.Sprintf(`{"wasm":{"contract":"%s","msg":%s}}`, contractAddress, `{"increment":{}}`),
+	}
+
+	// Initial transfer. Account is created by the wasm execute is not so we must do this twice to properly set up
+	transferTx, err := chainA.SendIBCTransfer(ctx, channel.ChannelID, user1.KeyName(), transfer, memo)
+	require.NoError(t, err)
+	chainAHeight, err := chainA.Height(ctx)
+	require.NoError(t, err)
+
+	_, err = testutil.PollForAck(ctx, chainA, chainAHeight-5, chainAHeight+55, transferTx.Packet)
+	require.NoError(t, err)
+
+}
+
+func setupCounterContract(ctx context.Context, chainB *cosmos.CosmosChain, user2 ibc.Wallet, t *testing.T) string {
 	codeId, err := chainB.StoreContract(ctx, user2.KeyName(), "artifacts/contracts/ibchooks_counter.wasm")
 	if err != nil {
 		require.NoError(t, err)
 	}
 
 	initMsg := `{"count":0}`
-	contractAddr, err := chainB.InstantiateContract(ctx, user2.KeyName(), codeId, initMsg, true)
-	if err != nil {
-		require.NoError(t, err)
+	cmd := []string{
+		chainB.Config().Bin, "tx", "wasm", "instantiate", codeId, initMsg,
+		"--label", "test counter contract",
+		"--no-admin",
+		"--node", chainB.GetRPCAddress(),
+		"--home", chainB.HomeDir(),
+		"--chain-id", chainB.Config().ChainID,
+		"--from", user2.KeyName(),
+		"--gas", "500000",
+		"--keyring-dir", chainB.HomeDir(),
+		"--keyring-backend", keyring.BackendTest,
+		"-b", "block",
+		"-y",
 	}
+	_, _, err = chainB.Exec(ctx, cmd, nil)
+	require.NoError(t, err, "could not instantiate the contract")
 
-	panic(contractAddr)
-
-	// channel, err := ibc.GetTransferChannel(ctx, r, eRep, chainA.Config().ChainID, chainB.Config().ChainID)
-	// require.NoError(t, err)
-
-	// transfer := ibc.WalletAmount{
-	// 	Address: contractAddr,
-	// 	Denom:   chainA.Config().Denom,
-	// 	Amount:  int64(1),
-	// }
-
-	// memo := ibc.TransferOptions{
-	// 	Memo: fmt.Sprintf(`{"wasm":{"contract":"%s","msg":%s}}`, contractAddr, `{"increment":{}}`),
-	// }
-
-	// // Initial transfer. Account is created by the wasm execute is not so we must do this twice to properly set up
-	// transferTx, err := chainA.SendIBCTransfer(ctx, channel.ChannelID, user1.KeyName(), transfer, memo)
-	// require.NoError(t, err)
-	// chainAHeight, err := chainA.Height(ctx)
-	// require.NoError(t, err)
-
+	cmd = []string{
+		chainB.Config().Bin, "q", "wasm", "list-contract-by-code", codeId,
+		"--node", chainB.GetRPCAddress(),
+		"--home", chainB.HomeDir(),
+		"--chain-id", chainB.Config().ChainID,
+	}
+	stdout, _, err := chainB.Exec(ctx, cmd, nil)
+	require.NoError(t, err, "could not list the contracts")
+	contactsRes := cosmos.QueryContractResponse{}
+	err = yaml.Unmarshal(stdout, &contactsRes)
+	require.NoError(t, err, "could not unmarshal query contract response")
+	contractAddress := contactsRes.Contracts[0]
+	return contractAddress
 }
