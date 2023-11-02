753446d703c7f09e39259404a2b42b9916671475
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2023-11-02 18:23:23 +0000

    implement init/export genesis

--- x/authority/genesis.go
+++ x/authority/genesis.go
@@ -6,12 +6,20 @@ import (
 	"github.com/public-awesome/stargaze/v13/x/authority/types"
 )
 
-// InitGenesis initializes the capability module's state from a provided genesis
+// InitGenesis initializes the authority module's state from a provided genesis
 // state.
 func InitGenesis(ctx sdk.Context, k keeper.Keeper, genState types.GenesisState) {
+	params := genState.Params
+	err := k.SetParams(ctx, params)
+	if err != nil {
+		panic(err)
+	}
 }
 
-// ExportGenesis returns the capability module's exported genesis.
+// ExportGenesis returns the authority module's exported genesis.
 func ExportGenesis(ctx sdk.Context, k keeper.Keeper) *types.GenesisState {
-	return &types.GenesisState{}
+	params := k.GetParams(ctx)
+	return &types.GenesisState{
+		Params: params,
+	}
 }
