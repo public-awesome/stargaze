ec2df284ce8c6b76e584c8c2a19aefdbd553ae28
jhernandezb <contact@jhernandez.me>
2023-11-20 09:07:34 -0600

    fix location

--- scripts/ci/upgrade/setup-preinstalled-stargaze.sh
+++ scripts/ci/upgrade/setup-preinstalled-stargaze.sh
@@ -16,8 +16,8 @@ sed -i 's/pruning = "syncable"/pruning = "nothing"/g' $STARGAZE_HOME/config/app.
 sed -i 's/enable = false/enable = true/g' $STARGAZE_HOME/config/app.toml
 sed -i 's/localhost:9090/0.0.0.0:9090/g' $STARGAZE_HOME/config/app.toml
 sed -i 's/172800s/60s/g'  $STARGAZE_HOME/config/genesis.json
-sed -i -e 's/timeout_commit = "5s"/timeout_commit = "100ms"/g' ~/.starsd/config/config.toml
-sed -i -e 's/timeout_propose = "3s"/timeout_propose = "100ms"/g' ~/.starsd/config/config.toml
+sed -i -e 's/timeout_commit = "5s"/timeout_commit = "100ms"/g'  $STARGAZE_HOME/config/config.toml
+sed -i -e 's/timeout_propose = "3s"/timeout_propose = "100ms"/g'  $STARGAZE_HOME/config/config.toml
 starsd keys --keyring-backend test add validator --home $STARGAZE_HOME
 starsd keys --keyring-backend test add funder --home $STARGAZE_HOME
 starsd add-genesis-account $(starsd keys --keyring-backend test show validator -a --home $STARGAZE_HOME) 10000000000000$DENOM --home $STARGAZE_HOME
