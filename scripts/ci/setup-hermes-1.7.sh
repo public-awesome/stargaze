48e9c34e30ab9a2169a634269c1d69f71147acee
jhernandezb <contact@jhernandez.me>
2023-12-26 11:33:47 -0600

    upgrade hermes

---
+++ scripts/ci/setup-hermes-1.7.sh
@@ -0,0 +1,13 @@
+set -ex
+mkdir -p ~/.hermes/
+cp ./scripts/ci/hermes/v1.7/config.toml ~/.hermes/
+hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
+hermes keys add gaia -f $PWD/scripts/ci/hermes/gaia.json
+hermes keys add osmosis -f $PWD/scripts/ci/hermes/osmosis.json
+hermes keys add icad -f $PWD/scripts/ci/hermes/icad.json
+hermes create connection stargaze gaia
+hermes create connection stargaze osmosis
+hermes create connection stargaze icad
+hermes create channel --port-a transfer --port-b transfer stargaze connection-0
+hermes create channel --port-a transfer --port-b transfer stargaze connection-1
+
