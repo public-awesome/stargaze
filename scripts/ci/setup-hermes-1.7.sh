49bc4fb9e9c6041ffc1c82ba2eac79049f131c80
jhernandezb <contact@jhernandez.me>
2023-12-26 12:36:27 -0600

    fix connection creation

--- scripts/ci/setup-hermes-1.7.sh
+++ scripts/ci/setup-hermes-1.7.sh
@@ -5,9 +5,9 @@ hermes keys add --chain stargaze --key-name relayer --key-file $PWD/scripts/ci/h
 hermes keys add --chain gaia --key-name relayer --key-file $PWD/scripts/ci/hermes/gaia.json
 hermes keys add --chain osmosis --key-name relayer --key-file $PWD/scripts/ci/hermes/osmosis.json
 hermes keys add --chain icad --key-name relayer --key-file $PWD/scripts/ci/hermes/icad.json
-hermes create connection stargaze gaia
-hermes create connection stargaze osmosis
-hermes create connection stargaze icad
-hermes create channel --port-a transfer --port-b transfer stargaze connection-0
-hermes create channel --port-a transfer --port-b transfer stargaze connection-1
+hermes create connection --a-chain stargaze --b-chain gaia
+hermes create connection --a-chain stargaze --b-chain osmosis
+hermes create connection --a-chain stargaze --b-chain icad
+hermes create channel --port-a transfer --port-b transfer --a-chain stargaze connection-0
+hermes create channel --port-a transfer --port-b transfer --a-chain stargaze connection-1
 
