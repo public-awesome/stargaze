ee565f19dc4b1a6a744ec1ec4982b89bc5290284
jhernandezb <contact@jhernandez.me>
2023-12-26 12:26:53 -0600

    fix

--- scripts/ci/setup-hermes-1.7.sh
+++ scripts/ci/setup-hermes-1.7.sh
@@ -1,7 +1,6 @@
 set -ex
 mkdir -p ~/.hermes/
 cp ./scripts/ci/hermes/v1.7/config.toml ~/.hermes/
-hermes keys add stargaze -f $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add --chain stargaze --key-name relayer --key-file $PWD/scripts/ci/hermes/stargaze.json
 hermes keys add --chain gaia --key-name relayer --key-file $PWD/scripts/ci/hermes/gaia.json
 hermes keys add --chain osmosis --key-name relayer --key-file $PWD/scripts/ci/hermes/osmosis.json
