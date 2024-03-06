c18ef822e222067d7cc483570dfd056245e63e75
jhernandezb <contact@jhernandez.me>
2024-03-06 11:38:23 -0600

    bump upgrade name

--- app/upgrades.go
+++ app/upgrades.go
@@ -29,7 +29,7 @@ import (
 )
 
 // next upgrade name
-const upgradeName = "v13-rc5-testnet"
+const upgradeName = "v13-rc6-testnet"
 
 // RegisterUpgradeHandlers returns upgrade handlers
 func (app *App) RegisterUpgradeHandlers(cfg module.Configurator) {
