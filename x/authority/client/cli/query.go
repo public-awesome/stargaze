de9bfc0800bbf05a8b6f8aec969e118f60111687
Spoorthi Satheesha <9302666+spoo-bar@users.noreply.github.com>
2024-04-19 16:16:46 +0530

    linting 🧹

--- x/authority/client/cli/query.go
+++ x/authority/client/cli/query.go
@@ -33,7 +33,7 @@ func GetCmdParams() *cobra.Command {
 		Use:   "params",
 		Short: "Query the module params",
 		Args:  cobra.NoArgs,
-		RunE: func(cmd *cobra.Command, args []string) error {
+		RunE: func(cmd *cobra.Command, _ []string) error {
 			clientCtx, err := client.GetClientQueryContext(cmd)
 			if err != nil {
 				return err
