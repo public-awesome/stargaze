23af97e9fc27dc51496cadb1e70a368804295909
jhernandezb <contact@jhernandez.me>
2024-05-22 21:09:05 -0600

    append to default template

--- app/params/config.go
+++ app/params/config.go
@@ -1,6 +1,7 @@
 package params
 
 import (
+	"fmt"
 	"time"
 
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
@@ -15,8 +16,36 @@ type CustomAppConfig struct {
 	Oracle oracleconfig.AppConfig `mapstructure:"oracle"`
 }
 
-func CustomconfigTemplate(config wasmtypes.WasmConfig) string {
-	return serverconfig.DefaultConfigTemplate + wasmtypes.ConfigTemplate(config)
+// ConfigTemplate toml snippet for app.toml
+func OracleConfigTemplate(c oracleconfig.AppConfig) string {
+
+	return fmt.Sprintf(`
+###############################################################################
+###                                  Oracle                                 ###
+###############################################################################
+[oracle]
+# Enabled indicates whether the oracle is enabled.
+enabled = %t
+
+# Oracle Address is the URL of the out of process oracle sidecar. This is used to
+# connect to the oracle sidecar when the application boots up. Note that the address
+# can be modified at any point, but will only take effect after the application is
+# restarted. This can be the address of an oracle container running on the same
+# machine or a remote machine.
+oracle_address = "%s"
+
+# Client Timeout is the time that the client is willing to wait for responses from 
+# the oracle before timing out.
+client_timeout = "%s"
+
+# MetricsEnabled determines whether oracle metrics are enabled. Specifically
+# this enables instrumentation of the oracle client and the interaction between
+# the oracle and the app.
+metrics_enabled = %t
+`, c.Enabled, c.OracleAddress, c.ClientTimeout.String(), c.MetricsEnabled)
+}
+func CustomconfigTemplate(config wasmtypes.WasmConfig, oracleConfig oracleconfig.AppConfig) string {
+	return serverconfig.DefaultConfigTemplate + wasmtypes.ConfigTemplate(config) + OracleConfigTemplate(oracleConfig)
 }
 
 func DefaultConfig() (string, interface{}) {
@@ -41,5 +70,5 @@ func DefaultConfig() (string, interface{}) {
 		},
 	}
 
-	return CustomconfigTemplate(wasmConfig), customConfig
+	return CustomconfigTemplate(wasmConfig, customConfig.Oracle), customConfig
 }
