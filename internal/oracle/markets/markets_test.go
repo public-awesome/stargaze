4804e81089504717c7604897f03f2c6a838a3569
jhernandezb <contact@jhernandez.me>
2024-05-27 00:38:48 -0600

    add market dependency

--- internal/oracle/markets/markets_test.go
+++ internal/oracle/markets/markets_test.go
@@ -10,12 +10,13 @@ import (
 func TestMarkets(t *testing.T) {
 	m, err := markets.Map()
 	require.NoError(t, err)
-	require.Len(t, m.Markets, 2)
+	require.Len(t, m.Markets, 3)
 
 	s, err := markets.Slice()
 	require.NoError(t, err)
-	require.Len(t, s, 2)
+	require.Len(t, s, 3)
 
 	require.Equal(t, "ATOM/USD", s[0].Ticker.String())
 	require.Equal(t, "BTC/USD", s[1].Ticker.String())
+	require.Equal(t, "USDT/USD", s[2].Ticker.String())
 }
