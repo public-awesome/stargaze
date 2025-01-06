14e345994cd339602a13033ac396fcc1062d2979
jhernandezb <contact@jhernandez.me>
2025-01-06 13:50:46 -0600

    update balances query

--- scripts/ci/upgrade/transfer-relayer.sh
+++ scripts/ci/upgrade/transfer-relayer.sh
@@ -18,5 +18,5 @@ export GAIA_ADDRESS=cosmos1wt3khka7cmn5zd592x430ph4zmlhf5gfztgha6
 export STARGAZE_ADDRESS=stars12g0xe2ld0k5ws3h7lmxc39d4rpl3fyxp5qys69
 export OSMOSIS_ADDRESS=osmo1qk2rqkk28z8v3d7npupz33zqc6dae6n9a2x5v4
 curl -s http://gaia:1317/bank/balances/$GAIA_ADDRESS | jq '.'
-curl -s http://stargaze-upgraded:1317/bank/balances/$STARGAZE_ADDRESS | jq '.'
+curl -s http://stargaze-upgraded:1317/cosmos/bank/v1beta1/balances/$STARGAZE_ADDRESS | jq '.'
 curl -s http://osmosis:1317/bank/balances/$OSMOSIS_ADDRESS | jq '.'
