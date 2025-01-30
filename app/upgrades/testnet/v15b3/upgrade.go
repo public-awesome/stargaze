a6a003441762c932cc9e523102c2acb2ec56ad8a
jhernandezb <contact@jhernandez.me>
2025-01-29 21:00:36 -0600

    add testnet v15b3 upgrade

---
+++ app/upgrades/testnet/v15b3/upgrade.go
@@ -0,0 +1,36 @@
+package v15
+
+import (
+	"context"
+	"time"
+
+	storetypes "cosmossdk.io/store/types"
+	upgradetypes "cosmossdk.io/x/upgrade/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/module"
+	"github.com/public-awesome/stargaze/v15/app/keepers"
+	"github.com/public-awesome/stargaze/v15/app/upgrades"
+)
+
+// next upgrade name
+const UpgradeName = "v15b3"
+
+var Upgrade = upgrades.Upgrade{
+	UpgradeName: UpgradeName,
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator, _ keepers.StargazeKeepers) upgradetypes.UpgradeHandler {
+		return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			startTime := time.Now()
+			wctx := sdk.UnwrapSDKContext(ctx)
+			wctx.Logger().Info("upgrade started", "upgrade_name", UpgradeName)
+			migrations, err := mm.RunMigrations(ctx, cfg, fromVM)
+			if err != nil {
+				return nil, err
+			}
+			wctx.Logger().Info("upgrade completed", "duration_ms", time.Since(startTime).Milliseconds())
+			return migrations, nil
+		}
+	},
+	StoreUpgrades: storetypes.StoreUpgrades{
+		Added: []string{},
+	},
+}
