8a1745c5f03c9d512a15daac86c6875f364c6246
Jorge Hernandez <3452489+jhernandezb@users.noreply.github.com>
2025-02-11 12:48:57 -0600

    global fee impr (#1209)
    * bump comet (#1203)
    
    * globalfee: set gas restrictions
    
    * update default settings
    
    * add more test cases
    
    * add test

--- x/globalfee/ante/fee_test.go
+++ x/globalfee/ante/fee_test.go
@@ -27,13 +27,18 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 	counterIncrementMsg := []byte(`{"increment": {}}`)
 	counterResetMsg := []byte(`{"reset": 0}`)
 
+	strPtr := func(s string) *string {
+		return &s
+	}
 	testCases := []struct {
-		testCase    string
-		minGasPrice sdk.DecCoins // min gas price configured by the validator
-		globalFees  sdk.DecCoins // minimum gas price configured by x/globalfee module param
-		feeSent     sdk.Coins    // the amount of fee sent by the user in the tx
-		msg         []sdk.Msg
-		expectErr   bool
+		testCase           string
+		minGasPrice        sdk.DecCoins // min gas price configured by the validator
+		globalFees         sdk.DecCoins // minimum gas price configured by x/globalfee module param
+		feeSent            sdk.Coins    // the amount of fee sent by the user in the tx
+		msg                []sdk.Msg
+		expectErr          bool
+		gasWanted          int64
+		expectedConainsErr *string
 	}{
 		{
 			"fail: min_gas_price: empty, globalfee: 5stake, feeSent: 1stake, not authorized contract exec",
@@ -48,6 +53,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: empty, globalfee: 5stake, feeSent: 7stake, not authorized contract exec",
@@ -62,6 +69,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"fail: min_gas_price: 0stake, globalfee: 5stake, feeSent: 0stake, not authorized contract exec",
@@ -76,6 +85,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 0stake, globalfee: 5stake, feeSent: 5stake, not authorized contract exec",
@@ -90,6 +101,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 1stake, not authorized contract exec",
@@ -104,6 +117,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 3stake, not authorized contract exec",
@@ -118,6 +133,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 5stake, not authorized contract exec",
@@ -132,6 +149,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized code id",
@@ -146,6 +165,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized contract address but not auth msg",
@@ -160,6 +181,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized contract address with auth msg",
@@ -174,6 +197,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized contract address with auth all (*)",
@@ -188,6 +213,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized contract address with auth all (*)",
@@ -202,6 +229,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, multiple authorized contract calls",
@@ -226,6 +255,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
 		},
 		{
 			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, one authorized contract + unauthorized msgs",
@@ -250,6 +281,8 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			true,
+			1,
+			nil,
 		},
 		{
 			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, one authorized contract + unauthorized msgs",
@@ -269,6 +302,125 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				},
 			},
 			false,
+			1,
+			nil,
+		},
+		{
+			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, authorized code id but overallocated gas",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)),
+			[]sdk.Msg{
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterIncrementMsg,
+				},
+			},
+			true,
+			50_000_000,
+			strPtr("overallocated gas value"),
+		},
+		{
+			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, mixed authz and overallocated gas ",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)),
+			[]sdk.Msg{
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterIncrementMsg,
+				},
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterResetMsg,
+				},
+			},
+			true,
+			50_000_000,
+			strPtr("overallocated gas value"),
+		},
+		{
+			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 500STAKE, mixed authz and overallocated gas  ",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 500_000_000)),
+			[]sdk.Msg{
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterIncrementMsg,
+				},
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterResetMsg,
+				},
+			},
+			true,
+			50_000_000,
+			strPtr("overallocated gas value"),
+		},
+		{
+			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 0stake, mixed authz and overallocated gas inv first",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 0)),
+			[]sdk.Msg{
+
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterResetMsg,
+				},
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterIncrementMsg,
+				},
+			},
+			true,
+			50_000_000,
+			strPtr("overallocated gas value"),
+		},
+		{
+			"fail: min_gas_price: 2stake, globalfee: 5stake, feeSent: 500STAKE, mixed authz and overallocated gas inv first message",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 500_000_000)),
+			[]sdk.Msg{
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterResetMsg,
+				},
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterIncrementMsg,
+				},
+			},
+			true,
+			50_000_000,
+			strPtr("overallocated gas value"),
+		},
+		{
+			"ok: min_gas_price: 2stake, globalfee: 5stake, feeSent: 500STAKE, unauthorized contract exec",
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 2)),
+			sdk.NewDecCoins(sdk.NewInt64DecCoin(sdk.DefaultBondDenom, 5)),
+			sdk.NewCoins(sdk.NewInt64Coin(sdk.DefaultBondDenom, 500_000_000)),
+			[]sdk.Msg{
+				&wasmtypes.MsgExecuteContract{
+					Sender:   addr1.String(),
+					Contract: contractWithCodeAuth,
+					Msg:      counterResetMsg,
+				},
+			},
+			false,
+			50_000_000,
+			nil,
 		},
 	}
 
@@ -277,7 +429,7 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 			_, antehandler := s.SetupTestGlobalFeeStoreAndMinGasPrice(tc.minGasPrice, tc.globalFees)
 			s.Require().NoError(s.txBuilder.SetMsgs(tc.msg...))
 			s.txBuilder.SetFeeAmount(tc.feeSent)
-			s.txBuilder.SetGasLimit(1)
+			s.txBuilder.SetGasLimit(uint64(tc.gasWanted))
 			tx, err := s.CreateTestTx(s.ctx, privs, accNums, accSeqs, s.ctx.ChainID(), signing.SignMode_SIGN_MODE_DIRECT)
 			s.Require().NoError(err)
 
@@ -287,6 +439,9 @@ func (s *AnteHandlerTestSuite) TestFeeDecoratorAntehandler() {
 				s.Require().NoError(err)
 			} else {
 				s.Require().Error(err)
+				if tc.expectedConainsErr != nil {
+					s.Require().Contains(err.Error(), *tc.expectedConainsErr)
+				}
 			}
 		})
 	}
