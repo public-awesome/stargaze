9edbb98509e6bc42af4692a569ce4d8cc772bd93
jhernandezb <contact@jhernandez.me>
2025-02-21 13:21:44 -0600

    updates

--- cmd/starsd/cmd/start.go
+++ cmd/starsd/cmd/start.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var LibwasmVersion = "2.1.5"
+var LibwasmVersion = "2.2.2"
 
 func CheckLibwasmVersion(_ *cobra.Command, _ []string) error {
 	version, err := wasmvmapi.LibwasmvmVersion()
