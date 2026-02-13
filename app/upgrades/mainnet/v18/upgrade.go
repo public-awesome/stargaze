af62b9c79b7988dbbd40f72dd88d4f98cd5d1cdd
ghost-builder <ghosttybits@proton.me>
2026-02-13 08:35:27 -0600

    add authorized address

--- app/upgrades/mainnet/v18/upgrade.go
+++ app/upgrades/mainnet/v18/upgrade.go
@@ -10,6 +10,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/module"
 	"github.com/public-awesome/stargaze/v17/app/keepers"
 	"github.com/public-awesome/stargaze/v17/app/upgrades"
+	pausertypes "github.com/public-awesome/stargaze/v17/x/pauser/types"
 )
 
 // next upgrade name
@@ -17,7 +18,7 @@ const UpgradeName = "v18"
 
 var Upgrade = upgrades.Upgrade{
 	UpgradeName: UpgradeName,
-	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator, _ keepers.StargazeKeepers) upgradetypes.UpgradeHandler {
+	CreateUpgradeHandler: func(mm *module.Manager, cfg module.Configurator, keepers keepers.StargazeKeepers) upgradetypes.UpgradeHandler {
 		return func(ctx context.Context, _ upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			startTime := time.Now()
 			wctx := sdk.UnwrapSDKContext(ctx)
@@ -26,6 +27,17 @@ var Upgrade = upgrades.Upgrade{
 			if err != nil {
 				return nil, err
 			}
+
+			// Set initial pauser module params with authorized address
+			pauserParams := pausertypes.Params{
+				PrivilegedAddresses: []string{
+					"stars1s8qx0zvz8yd6e4x0mqmqf7fr9vvfn622wtp3g3",
+				},
+			}
+			if err := keepers.PauserKeeper.SetParams(wctx, pauserParams); err != nil {
+				return nil, err
+			}
+
 			wctx.Logger().Info("upgrade completed", "duration_ms", time.Since(startTime).Milliseconds())
 			return migrations, nil
 		}
